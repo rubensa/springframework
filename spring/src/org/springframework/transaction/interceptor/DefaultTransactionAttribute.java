b2388bd4691d39d2a5b772c30229c828a1942305
jhoeller <>
2003-08-18 16:16:48 +0000

    added toString/equals/hashCode

--- spring/src/org/springframework/transaction/interceptor/DefaultTransactionAttribute.java
+++ spring/src/org/springframework/transaction/interceptor/DefaultTransactionAttribute.java
@@ -14,8 +14,13 @@ import org.springframework.transaction.support.DefaultTransactionDefinition;
  * @since 16-Mar-2003
  * @version $Id$
  */
-public class DefaultTransactionAttribute extends DefaultTransactionDefinition
-    implements TransactionAttribute {
+public class DefaultTransactionAttribute extends DefaultTransactionDefinition implements TransactionAttribute {
+
+	/** Prefix for rollback-on-exception rules in description strings */
+	public static final String ROLLBACK_RULE_PREFIX = "-";
+
+	/** Prefix for commit-on-exception rules in description strings */
+	public static final String COMMIT_RULE_PREFIX = "+";
 
 	public DefaultTransactionAttribute() {
 	}
@@ -30,6 +35,19 @@ public class DefaultTransactionAttribute extends DefaultTransactionDefinition
 	 */
 	public boolean rollbackOn(Throwable t) {
 		return (t instanceof RuntimeException);
+	}
+
+	/**
+	 * Return a description of this transaction attribute.
+	 * The format matches the one used by TransactionAttributeEditor,
+	 * to be able to feed toString results into TransactionAttribut properties.
+	 * @see org.springframework.transaction.interceptor.TransactionAttributeEditor
+	 */
+	public String toString() {
+		StringBuffer result = getDefinitionDescription();
+		result.append(',');
+		result.append(ROLLBACK_RULE_PREFIX + "RuntimeException");
+		return result.toString();
 	}
 
 }

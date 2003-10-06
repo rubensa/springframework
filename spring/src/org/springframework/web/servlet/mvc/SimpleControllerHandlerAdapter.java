ec1a8ca4a04926c95f0ede50983be095fc458134
jhoeller <>
2003-10-06 14:27:28 +0000

    moved LastModified interface to org.springframework.web.servlet.mvc

--- spring/src/org/springframework/web/servlet/mvc/SimpleControllerHandlerAdapter.java
+++ spring/src/org/springframework/web/servlet/mvc/SimpleControllerHandlerAdapter.java
@@ -8,16 +8,19 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.context.support.WebApplicationObjectSupport;
 import org.springframework.web.servlet.HandlerAdapter;
-import org.springframework.web.servlet.LastModified;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
- * Adapter to use the Controller workflow interface with the generic
- * DispatcherServlet. This is an SPI class, not used directly by
- * application code.
+ * Adapter to use the Controller workflow interface with the generic DispatcherServlet.
+ * Supports controllers that implement the LastModified interface.
+ *
+ * <p>This is an SPI class, not used directly by application code.
+ *
  * @author Rod Johnson
- * @see org.springframework.web.servlet.DispatcherServlet
  * @version $Id$
+ * @see org.springframework.web.servlet.DispatcherServlet
+ * @see Controller
+ * @see LastModified
  */
 public class SimpleControllerHandlerAdapter extends WebApplicationObjectSupport implements HandlerAdapter {
 	

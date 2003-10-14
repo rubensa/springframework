f567b1a4c8bf0c0d1d6848dc6a8f6e711022026c
johnsonr <>
2003-10-14 18:56:46 +0000

    Added convenience method to check whether a pointcut can ever apply. Can allow more efficient implementation of framework.

--- spring/src/org/springframework/aop/framework/support/AopUtils.java
+++ spring/src/org/springframework/aop/framework/support/AopUtils.java
@@ -5,6 +5,9 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.aopalliance.intercept.AttributeRegistry;
+import org.springframework.aop.framework.StaticMethodPointcut;
+
 /**
  * Utility methods used by the AOP framework.
  * @author Rod Johnson
@@ -65,6 +68,33 @@ public class AopUtils {
 				// Didn't find it...keep going
 			}
 		}	// for
+		return false;
+	}
+
+	/**
+	 * Can the given pointcut apply at all on the given class?
+	 * This is an important test as it can be used to optimize
+	 * out a pointcut for a class
+	 * @param pc pc static or dynamic pointcut
+	 * @param ar AttributeRegistry
+	 * @param clazz class we're testing
+	 * @param proxyInterfaces proxy interfaces. If null, all methods
+	 * on class may be proxied
+	 * @return whether the pointcut can apply on any method
+	 */
+	public static boolean canApply(StaticMethodPointcut pc, AttributeRegistry ar, Class clazz, Class[] proxyInterfaces) {
+		// Check whether it can apply on any method
+		Method[] methods = clazz.getDeclaredMethods();
+		for (int i = 0; i < methods.length; i++) {
+			Method m = methods[i];
+			// If we're looking only at interfaces and this method
+			// isn't on any of them, skip it
+			if (proxyInterfaces != null && !methodIsOnOneOfTheseInterfaces(m, proxyInterfaces)) {
+				continue;
+			}
+			if (pc.applies(m, ar))
+				return true;
+		}
 		return false;
 	}
 

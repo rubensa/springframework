524edd69c052d50b0073d3c2dbbabcd6bd01a83c
johnsonr <>
2003-11-16 21:53:11 +0000

    Improved Javadoc

--- spring/src/org/springframework/aop/InterceptionAroundAdvisor.java
+++ spring/src/org/springframework/aop/InterceptionAroundAdvisor.java
@@ -8,15 +8,19 @@ package org.springframework.aop;
 import org.aopalliance.intercept.Interceptor;
 
 
-
 /**
- *
+ * Advisor that delivers <b>around</b> advice via an AOP Alliance interceptor.
+ * Such advice is targeted by a pointcut.
  * @author Rod Johnson
  * @since 04-Apr-2003
  * @version $Id$
  */
 public interface InterceptionAroundAdvisor extends InterceptionAdvisor, PointcutAdvisor {
 	
+	/**
+	 * @return the pointcut that identifies joinpoints eligible for this
+	 * around advice.
+	 */
 	Interceptor getInterceptor(); 
 	
 
--- spring/src/org/springframework/aop/InterceptionIntroductionAdvisor.java
+++ spring/src/org/springframework/aop/InterceptionIntroductionAdvisor.java
@@ -5,21 +5,32 @@
  
 package org.springframework.aop;
 
-
-
-
 /**
- * TODO base Advice interface? ONLY WAY TO DO INTRODUCTION
+ * Advisor that performs an AOP <b>introduction</b> through an interceptor.
+ * Introduction is the implementation of additional interfaces 
+ * (not implemented by a target) via AOP advice.
  * @author Rod Johnson
  * @since 04-Apr-2003
+ * @see org.springframework.aop.IntroductionInterceptor
  * @version $Id$
  */
 public interface InterceptionIntroductionAdvisor extends InterceptionAdvisor {
 	
+	/**
+	 * @return the filter determining which target classes this introduction
+	 * should apply to. The class part of a pointcut. Note that method matching
+	 * doesn't make sense to introductions.
+	 */
 	ClassFilter getClassFilter();
 	
+	/**
+	 * @return the interceptor that handles the introduced interface(s)
+	 */
 	IntroductionInterceptor getIntroductionInterceptor();
 	
+	/**
+	 * @return the additional interfaces introduced by this Advisor
+	 */
 	Class[] getInterfaces();
 	
 

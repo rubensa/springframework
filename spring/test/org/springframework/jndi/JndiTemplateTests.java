f81f8cce2fad53ea5264768bbe5767e04b3a0afc
johnsonr <>
2003-11-20 15:20:43 +0000

    Added 2 lookup tests

--- spring/test/org/springframework/jndi/JndiTemplateTests.java
+++ spring/test/org/springframework/jndi/JndiTemplateTests.java
@@ -28,6 +28,55 @@ public class JndiTemplateTests extends TestCase {
 		super(arg0);
 	}
 	
+	public void testLookupSucceeds() throws Exception {
+		Object o = new Object();
+		String name = "foo";
+		MockControl mc = MockControl.createControl(Context.class);
+		final Context mock = (Context) mc.getMock();
+		mock.lookup(name);
+		mc.setReturnValue(o);
+		mock.close();
+		mc.setVoidCallable(1);
+		mc.replay();
+
+		JndiTemplate jt = new JndiTemplate() {
+			protected Context createInitialContext() throws NamingException {
+				return mock;
+			}
+		};
+
+		Object o2 = jt.lookup(name);
+		assertEquals(o, o2);
+		mc.verify();
+	}
+	
+	public void testLookupFails() throws Exception {
+		NamingException ne = new NamingException();
+		String name = "foo";
+		MockControl mc = MockControl.createControl(Context.class);
+		final Context mock = (Context) mc.getMock();
+		mock.lookup(name);
+		mc.setThrowable(ne);
+		mock.close();
+		mc.setVoidCallable(1);
+		mc.replay();
+
+		JndiTemplate jt = new JndiTemplate() {
+			protected Context createInitialContext() throws NamingException {
+				return mock;
+			}
+		};
+
+		try {
+			jt.lookup(name);
+			fail("Should have thrown NamingException");
+		}
+		catch (NamingException ex) {
+			// Ok
+		}
+		mc.verify();
+	}
+	
 	public void testBind() throws Exception {
 		Object o = new Object();
 		String name = "foo";

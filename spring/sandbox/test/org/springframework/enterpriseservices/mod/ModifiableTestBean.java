551d60b571af6b47819fd4eb6d2b60cdd4ac065d
johnsonr <>
2003-11-22 09:05:47 +0000

    Moved attributes and enterprise services here, and broke into src and test trees to allow proper testing

---
+++ spring/sandbox/test/org/springframework/enterpriseservices/mod/ModifiableTestBean.java
@@ -0,0 +1,15 @@
+package org.springframework.enterpriseservices.mod;
+
+import org.springframework.beans.TestBean;
+
+/**
+ * Extension of TestBean class to add a modifiable class attribute.
+ * @author Rod Johnson
+ * @version $Id$
+ * 
+ * @org.springframework.enterpriseservices.mod.Modifiable
+ */
+public class ModifiableTestBean extends TestBean {
+	
+
+}

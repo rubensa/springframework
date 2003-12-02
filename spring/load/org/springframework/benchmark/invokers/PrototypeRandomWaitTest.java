b241126d558df92ba3126623e8c7a42816ded9e3
johnsonr <>
2003-12-02 22:31:07 +0000

    First version

---
+++ spring/load/org/springframework/benchmark/invokers/PrototypeRandomWaitTest.java
@@ -0,0 +1,21 @@
+/*
+ * The Spring Framework is published under the terms
+ * of the Apache Software License.
+ */
+ 
+package org.springframework.benchmark.invokers;
+
+/**
+ * 
+ * @author Rod Johnson
+ * @version $Id$
+ */
+public class PrototypeRandomWaitTest extends RandomWaitTest {
+
+	/**
+	 * @see org.springframework.benchmark.invokers.RandomWaitTest#getService()
+	 */
+	protected Service getService() {
+		return (Service) bf.getBean(bean);
+	}
+}
---
+++ spring/load/org/springframework/benchmark/invokers/Service.java
@@ -0,0 +1,22 @@
+/*
+ * The Spring Framework is published under the terms
+ * of the Apache Software License.
+ */
+ 
+package org.springframework.benchmark.invokers;
+
+/**
+ * 
+ * @author Rod Johnson
+ * @version $Id$
+ */
+public interface Service {
+	
+	
+	void takeUpToMillis(int i);
+	
+	void nop();
+	
+	void notAdvised();
+
+}

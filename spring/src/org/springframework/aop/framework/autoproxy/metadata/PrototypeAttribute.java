8f7ccae5a13b7090107fb6661c6d7863c2d974b0
johnsonr <>
2003-12-15 10:07:08 +0000

    Added AttributesPrototypeTargetSourceCreator

---
+++ spring/src/org/springframework/aop/framework/autoproxy/metadata/PrototypeAttribute.java
@@ -0,0 +1,16 @@
+/*
+ * The Spring Framework is published under the terms
+ * of the Apache Software License.
+ */
+ 
+package org.springframework.aop.framework.autoproxy.metadata;
+
+/**
+ * Tag attribute to identify beans for which the target should be a 
+ * prototype (new instance per invocation)
+ * @author Rod Johnson
+ * @version $Id$
+ */
+public class PrototypeAttribute {
+
+}

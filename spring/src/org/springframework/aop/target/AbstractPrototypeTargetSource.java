543743c4f6617779b9b11e3a9a8eb5fda3b8d6ec
dkopylenko <>
2003-12-30 04:32:26 +0000

    Now implements InitializingBean to detect incoplete configuration early (required targetBeanName property) and avoid propogation of the lower level NoSuchBeanDefinitionException from AbstractBeanFactory to a client code which would make it harder to debug.

--- spring/src/org/springframework/aop/target/AbstractPrototypeTargetSource.java
+++ spring/src/org/springframework/aop/target/AbstractPrototypeTargetSource.java
@@ -21,8 +21,7 @@ import org.springframework.beans.factory.InitializingBean;
  * method to create a new prototype instance.
  * 
  * @author Rod Johnson
- * @version $Id: AbstractPrototypeTargetSource.java,v 1.1 2003/12/11 10:58:12
- *               johnsonr Exp $
+ * @version $Id$
  */
 public abstract class AbstractPrototypeTargetSource implements TargetSource, BeanFactoryAware, InitializingBean {
 

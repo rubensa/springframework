1d8703aa8731ea7b5a9ed6d28914d3e2a2ff3828
davison <>
2004-01-05 00:29:25 +0000

    *** empty log message ***

--- spring/autobuilds/build/autobuild.bat
+++ spring/autobuilds/build/autobuild.bat
@@ -56,11 +56,12 @@ set LISTENER=org.apache.tools.ant.XmlLogger
     if (%SERVER%) == () goto usage
     if (%APP%) == () goto usage
     if not exist %SERVER%-build.xml goto noserver
-    if not exist ..\..\samples\%APP% goto noapp
+    if not exist ..\..\samples\%APP%\.autobuilds goto noapp
     set BUILDLOG=..\..\target\autobuilds\reports\%APP%_%SERVER%_build_%yy%-%mm%-%dd%.log
 
 ::  ensure environment exists for 1st use
-    ant setup
+    ant -q setup
+    echo Please see the file %BUILDLOG% for build logging and unit test results
     ant -logger %LOGGER% -logfile %BUILDLOG% %ALLPROPS% main
     goto end
     
@@ -69,7 +70,7 @@ set LISTENER=org.apache.tools.ant.XmlLogger
     goto end
     
 :noapp
-    echo Can't find application %APP%
+    echo Can't find application %APP% or application is not configured for autobuilds
     goto end
     
 :usage
--- spring/autobuilds/src/org/springframework/apptests/AbstractTestCase.java
+++ spring/autobuilds/src/org/springframework/apptests/AbstractTestCase.java
@@ -5,7 +5,14 @@
 
 package org.springframework.apptests;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import junit.framework.TestCase;
+
+import org.apache.tools.ant.BuildException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 
 /**
@@ -16,13 +23,34 @@ import junit.framework.TestCase;
  */
 public abstract class AbstractTestCase extends TestCase {
 
-	protected String testServer = "http://localhost:" + System.getProperty("autobuilds.server.port");
+	//private static String jdbcPropsLocation = System.getProperty("autobuilds.hsqldb.conf.dir");
+	protected String testServer;
+	protected DriverManagerDataSource dataSource;
+	protected JdbcTemplate jdbcTemplate;	
+	
 	
     /**
      * @param arg0
      */
     public AbstractTestCase(String arg0) {
         super(arg0);
+		
+		try {
+			Properties props = new Properties();
+			props.load(new FileInputStream("build.properties"));
+			
+			dataSource = new DriverManagerDataSource();
+			dataSource.setDriverClassName(props.getProperty("autobuilds.jdbc.driver", "org.hsqldb.jdbcDriver"));
+			dataSource.setUrl(props.getProperty("autobuilds.jdbc.url","jdbc:hsqldb:hsql://localhost:9001"));
+			dataSource.setUsername(props.getProperty("autobuilds.jdbc.username", "sa")); 
+			dataSource.setPassword(props.getProperty("autobuilds.jdbc.password","")); 		
+			
+			jdbcTemplate = new JdbcTemplate(dataSource);
+			
+			testServer = "http://localhost:" + props.getProperty("autobuilds.server.http.port", "8080");
+			
+		} catch (Exception e) {
+			throw new BuildException("Failed to create dataSource or jdbcTemplate - are the drivers in $ANT_HOME/lib?  " + e);
+		}
     }
-
 }

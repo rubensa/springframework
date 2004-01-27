2e359eb4da46976fe5d3bf5aa7dafdf17dc4192a
colins <>
2004-01-27 18:29:17 +0000

    Fix SingletonBeanFactoryLocator/DefaultBeanFactoryLocator case where definition object could be created twice for same key

--- spring/src/org/springframework/context/access/DefaultBeanFactoryLocator.java
+++ spring/src/org/springframework/context/access/DefaultBeanFactoryLocator.java
@@ -37,7 +37,7 @@ public class DefaultBeanFactoryLocator extends SingletonBeanFactoryLocator {
 	public static final String BEANS_REFS_XML_NAME = "beanRefContext.xml";
 	
 	// the keyed singleton instances
-	private static SingletonBeanFactoryLocator _noSelectorInstance;
+	private static DefaultBeanFactoryLocator _noSelectorInstance;
 
 	private static HashMap instances = new HashMap();
 	
@@ -50,12 +50,7 @@ public class DefaultBeanFactoryLocator extends SingletonBeanFactoryLocator {
 	 * @throws FatalBeanException
 	 */
 	public static BeanFactoryLocator getInstance() throws FatalBeanException {
-
-		synchronized (instances) {
-			if (_noSelectorInstance == null)
-				_noSelectorInstance = new DefaultBeanFactoryLocator();
-			return _noSelectorInstance;
-		}
+		return getInstance(BEANS_REFS_XML_NAME);
 	}
 
 	/**
@@ -73,6 +68,10 @@ public class DefaultBeanFactoryLocator extends SingletonBeanFactoryLocator {
 			throws FatalBeanException {
 
 		synchronized (instances) {
+			_log.debug("DefaultBeanFactoryLocator.getInstance(): DefaultBeanFactoryLocator.class="
+				+ DefaultBeanFactoryLocator.class + "hash= " + DefaultBeanFactoryLocator.class.hashCode());
+			_log.debug("DefaultBeanFactoryLocator.getInstance(): instances.hashCode=" + instances.hashCode() + ", instances=" + instances);
+			
 			BeanFactoryLocator bfl = (BeanFactoryLocator) instances.get(selector);
 			if (bfl == null) {
 				bfl = new DefaultBeanFactoryLocator(selector);

4d19a8f15a20355dc317dd7475bab2d4b68a6c1c
jhoeller <>
2004-02-04 17:09:56 +0000

    attempt to rollback on Error

--- spring/src/org/springframework/transaction/interceptor/DefaultTransactionAttribute.java
+++ spring/src/org/springframework/transaction/interceptor/DefaultTransactionAttribute.java
@@ -8,7 +8,7 @@ package org.springframework.transaction.interceptor;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 /**
- * Transaction attribute that takes EJB approach to rolling
+ * Transaction attribute that takes the EJB approach to rolling
  * back on runtime, but not checked, exceptions.
  * @author Rod Johnson
  * @since 16-Mar-2003
@@ -30,17 +30,18 @@ public class DefaultTransactionAttribute extends DefaultTransactionDefinition im
 	}
 
 	/**
-	 * Default behaviour is as with EJB: rollback on unchecked exception.
+	 * Default behavior is as with EJB: rollback on unchecked exception.
+	 * Additionally attempt to rollback on Error.
 	 * Consistent with TransactionTemplate's behavior.
 	 */
-	public boolean rollbackOn(Throwable t) {
-		return (t instanceof RuntimeException);
+	public boolean rollbackOn(Throwable ex) {
+		return (ex instanceof RuntimeException || ex instanceof Error);
 	}
 
 	/**
 	 * Return a description of this transaction attribute.
 	 * The format matches the one used by TransactionAttributeEditor,
-	 * to be able to feed toString results into TransactionAttribut properties.
+	 * to be able to feed toString results into TransactionAttribute properties.
 	 * @see org.springframework.transaction.interceptor.TransactionAttributeEditor
 	 */
 	public String toString() {

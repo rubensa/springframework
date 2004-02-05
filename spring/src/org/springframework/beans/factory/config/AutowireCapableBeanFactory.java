e1b1a68030d01f9d7b3b757e3c289f8594983739
dkopylenko <>
2004-02-05 01:14:00 +0000

    JavaDoc polishing

--- spring/src/org/springframework/beans/factory/config/AutowireCapableBeanFactory.java
+++ spring/src/org/springframework/beans/factory/config/AutowireCapableBeanFactory.java
@@ -4,11 +4,12 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 
 /**
- * Extension of the ListableBeanFactory interface to be implemented by bean
+ * Extension of the BeanFactory interface to be implemented by bean
  * factories that are capable of autowiring and expose this functionality
  * for existing bean instances.
  * @author Juergen Hoeller
  * @since 04.12.2003
+ * $Id$
  */
 public interface AutowireCapableBeanFactory extends BeanFactory {
 

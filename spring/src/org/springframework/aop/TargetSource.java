eac2600ed997fcafaafc8007d43d51b412f410df
johnsonr <>
2004-02-22 09:59:59 +0000

    Better Javadoc

--- spring/src/org/springframework/aop/TargetSource.java
+++ spring/src/org/springframework/aop/TargetSource.java
@@ -6,7 +6,14 @@
 package org.springframework.aop;
 
 /**
- * 
+ * A TargetSource is used to obtain the current "target" of 
+ * an AOP invocation, which will be invoked via reflection if no
+ * around advice chooses to end the interceptor chain itself. 
+ * <br>If a TargetSource is "static", it will always
+ * return the same target, allowing optimizations in the AOP framework.
+ * Dynamic target sources can support pooling, hot swapping etc.
+ * <br>Application developers don't usually need to work with TargetSources
+ * directly: this is an AOP framework interface.
  * @author Rod Johnson
  * @version $Id$
  */

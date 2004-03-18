64d67e9b9aef180efc52f03f14aee6c65039c846
trisberg <>
2004-03-18 02:46:18 +0000

    Applied a consistent license header to all java source files

--- spring/src/org/springframework/aop/framework/AdvisedSupport.java
+++ spring/src/org/springframework/aop/framework/AdvisedSupport.java
@@ -1,7 +1,18 @@
 /*
- * The Spring Framework is published under the terms
- * of the Apache Software License.
- */
+ * Copyright 2002-2004 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */ 
 
 package org.springframework.aop.framework;
 
@@ -261,7 +272,8 @@ public class AdvisedSupport extends ProxyConfig implements Advised {
 		if (isFrozen())
 			throw new AopConfigException("Cannot remove Advisor: config is frozen");
 		if (index < 0 || index > advisors.size() - 1)
-			throw new AopConfigException("Advisor index " + index + " is out of bounds: " +					"Only have " + advisors.size() + " advisors");
+			throw new AopConfigException("Advisor index " + index + " is out of bounds: " +
+					"Only have " + advisors.size() + " advisors");
 		Advisor advisor = (Advisor) advisors.get(index);
 		if (advisor instanceof IntroductionAdvisor) {
 			IntroductionAdvisor ia = (IntroductionAdvisor) advisor;

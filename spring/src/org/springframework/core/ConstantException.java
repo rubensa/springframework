a2a0df38ca5250e9d5662a65e9233ad2a611dd81
jhoeller <>
2004-05-23 20:23:38 +0000

    hold class name rather than Class, for proper garbage collection

--- spring/src/org/springframework/core/ConstantException.java
+++ spring/src/org/springframework/core/ConstantException.java
@@ -28,22 +28,22 @@ public class ConstantException extends IllegalArgumentException {
 	
 	/**
 	 * Thrown when an invalid constant name is requested.
-	 * @param clazz class containing the constant definitions
+	 * @param className name of the class containing the constant definitions
 	 * @param field invalid constant name
 	 * @param message description of the problem
 	 */
-	public ConstantException(Class clazz, String field, String message) {
-		super("Field '" + field + "' " + message + " in " + clazz);
+	ConstantException(String className, String field, String message) {
+		super("Field '" + field + "' " + message + " in class [" + className + "]");
 	}
 
 	/**
 	 * Thrown when an invalid constant value is looked up.
-	 * @param clazz class containing the constant definitions
+	 * @param className name of the class containing the constant definitions
 	 * @param namePrefix prefix of the searched constant names
 	 * @param value the looked up constant value
 	 */
-	public ConstantException(Class clazz, String namePrefix, Object value) {
-		super("No '" + namePrefix + "' field with value '" + value + "' found in " + clazz);
+	ConstantException(String className, String namePrefix, Object value) {
+		super("No '" + namePrefix + "' field with value '" + value + "' found in class [" + className + "]");
 	}
 
 }

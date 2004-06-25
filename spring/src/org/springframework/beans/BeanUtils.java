208df0ba0b02a9a67aa41eef069044cf84e7103a
johnsonr <>
2004-06-25 09:11:36 +0000

    added isOverloadedMethod method to support lookup method validation. However, possibly of more general use.

--- spring/src/org/springframework/beans/BeanUtils.java
+++ spring/src/org/springframework/beans/BeanUtils.java
@@ -19,6 +19,7 @@ package org.springframework.beans;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
 import java.util.List;
@@ -195,6 +196,26 @@ public abstract class BeanUtils {
 			}
 		}
 		targetBw.setPropertyValues(values);
+	}
+	
+	/**
+	 * Is there one or more methods--with any argument types--
+	 * on this class or/and its superclasses? 
+	 * Includes non-public methods.
+	 */
+	public static boolean isAtLeastOneMethodWithName(String methodName, Class clazz) {
+		do {
+			for (int i = 0; i < clazz.getDeclaredMethods().length; i++) {
+				Method m = clazz.getDeclaredMethods()[i];
+				if (methodName.equals(m.getName())) {
+					//System.err.println(m + "; " + clazz);
+					return true;
+				}
+			}
+			clazz = clazz.getSuperclass();
+		}
+		while (clazz != null);
+		return false;
 	}
 
 }

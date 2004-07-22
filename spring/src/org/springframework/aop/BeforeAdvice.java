8d49a6b66b7dced3e56c9e6c92550f4aef3df751
jhoeller <>
2004-07-22 09:56:30 +0000

    polishing

--- spring/src/org/springframework/aop/BeforeAdvice.java
+++ spring/src/org/springframework/aop/BeforeAdvice.java
@@ -18,14 +18,13 @@ package org.springframework.aop;
 
 import org.aopalliance.aop.Advice;
 
-
 /**
  * Superinterface for all before advice. Spring supports only method before
  * advice. Although this is unlikely to change, this API is designed to
  * allow field advice in future if desired.
- * @see org.springframework.aop.MethodBeforeAdvice
  * @author Rod Johnson
  * @version $Id$
+ * @see org.springframework.aop.MethodBeforeAdvice
  */
 public interface BeforeAdvice extends Advice {
 

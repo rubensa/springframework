49228186bf36c294d77a5aa1b1d6d7c373e5b5ca
wgthom <>
2004-07-30 00:44:57 +0000

    polish javadocs

--- spring/sandbox/src/org/springframework/web/portlet/FrameworkPortlet.java
+++ spring/sandbox/src/org/springframework/web/portlet/FrameworkPortlet.java
@@ -51,10 +51,10 @@ import org.springframework.web.portlet.context.support.XmlPortletApplicationCont
  * successfully handled.
  * </ul>
  *
- * TODO update this with action and render service
- * <p>Subclasses must implement doService() to handle requests. Because this extends
- * PortletBean rather than Portlet directly, bean properties are mapped
- * onto it. Subclasses can override initFrameworkPortlet() for custom initialization.
+ * <p>Subclasses must implement doActionService() and doRenderService() to handle
+ * action and render requests. Because this extends PortletBean rather than Portlet
+ * directly, bean properties are mapped onto it. Subclasses can override
+ * initFrameworkPortlet() for custom initialization.
  *
  * <p>Regards a "contextClass" parameter at the portlet init-param level,
  * falling back to the default context class (XmlPortletApplicationContext) if not found.

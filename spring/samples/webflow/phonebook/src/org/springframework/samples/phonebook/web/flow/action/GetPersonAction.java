f629be44e203e286204bf45436a6cae324dd0ef1
kdonald <>
2005-02-21 06:32:51 +0000

    *** empty log message ***

--- spring/samples/webflow/phonebook/src/org/springframework/samples/phonebook/web/flow/action/GetPersonAction.java
+++ spring/samples/webflow/phonebook/src/org/springframework/samples/phonebook/web/flow/action/GetPersonAction.java
@@ -1,14 +1,15 @@
-package org.springframework.samples.phonebook.web.flow;
+package org.springframework.samples.phonebook.web.flow.action;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.samples.phonebook.domain.Person;
 import org.springframework.samples.phonebook.domain.PhoneBook;
-import org.springframework.samples.phonebook.domain.PhoneBookQuery;
+import org.springframework.samples.phonebook.domain.UserId;
 import org.springframework.web.flow.Action;
 import org.springframework.web.flow.MutableFlowModel;
 
-public class QueryAction implements Action {
+public class GetPersonAction implements Action {
 
 	private PhoneBook phoneBook;
 
@@ -18,8 +19,14 @@ public class QueryAction implements Action {
 
 	public String execute(HttpServletRequest request, HttpServletResponse response, MutableFlowModel model)
 			throws Exception {
-		PhoneBookQuery query = (PhoneBookQuery)model.getAttribute("query");
-		model.setAttribute("persons", phoneBook.query(query));
-		return "ok";
+		UserId userId = (UserId)model.getAttribute("id");
+		Person person = phoneBook.getPerson(userId);
+		if (person != null) {
+			model.setAttribute("person", person);
+			return "success";
+		}
+		else {
+			return "error";
+		}
 	}
 }

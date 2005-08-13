2080224b889070b41680ec25bd35b004fb073aa2
kdonald <>
2005-08-13 16:40:38 +0000

    jdk 1.5 boo boo

--- spring-projects/spring-webflow/src/java/org/springframework/webflow/action/bean/Arguments.java
+++ spring-projects/spring-webflow/src/java/org/springframework/webflow/action/bean/Arguments.java
@@ -2,7 +2,7 @@ package org.springframework.webflow.action.bean;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -72,7 +72,7 @@ public class Arguments implements Serializable {
 	 * @return the arguments
 	 */
 	public boolean addAll(Argument[] arguments) {
-		return Collections.addAll(this.arguments, arguments);
+		return this.arguments.addAll(Arrays.asList(arguments)); 
 	}
 
 	/**

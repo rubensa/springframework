f34f56d64c4ef909b44afa5847a4a7b69dc66cbf
klr8 <>
2005-08-17 07:51:43 +0000

    Code review.

--- spring-projects/spring-webflow-annotations/src/java/org/springframework/webflow/action/annotation/AnnotationBeanStatePersister.java
+++ spring-projects/spring-webflow-annotations/src/java/org/springframework/webflow/action/annotation/AnnotationBeanStatePersister.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2002-2005 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.webflow.action.annotation;
 
 import java.lang.reflect.Field;
@@ -28,9 +43,10 @@ public class AnnotationBeanStatePersister implements BeanStatePersister {
 		for (int i = 0; i < fields.length; i++) {
 			Field field = fields[i];
 			if (!field.isAnnotationPresent(Transient.class)) {
+				boolean accessible = field.isAccessible();
 				field.setAccessible(true);
 				memento.put(field.getName(), field.get(bean));
-				field.setAccessible(false);
+				field.setAccessible(accessible);
 			}
 		}
 	}
@@ -46,9 +62,10 @@ public class AnnotationBeanStatePersister implements BeanStatePersister {
 			for (int i = 0; i < fields.length; i++) {
 				Field field = fields[i];
 				if (!field.isAnnotationPresent(Transient.class)) {
+					boolean accessible = field.isAccessible();
 					field.setAccessible(true);
 					field.set(bean, memento.get(field.getName()));
-					field.setAccessible(false);
+					field.setAccessible(accessible);
 				}
 			}
 		}
--- spring-projects/spring-webflow-annotations/src/java/org/springframework/webflow/action/annotation/Stateful.java
+++ spring-projects/spring-webflow-annotations/src/java/org/springframework/webflow/action/annotation/Stateful.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2002-2005 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.webflow.action.annotation;
 
 import java.lang.annotation.Documented;
@@ -10,13 +25,13 @@ import java.lang.annotation.Target;
 /**
  * Declares that a bean has state that should be managed by Web Flow. Beans
  * marked stateful will automatically have all non
- * @Transient fields saved out to flow scope after each invocation. In addition,
+ * &#64;Transient fields saved out to flow scope after each invocation. In addition,
  * persistent fields will be restored from flow scope before any subsequent
  * invocations.
  * 
  * @author Keith Donald
  */
-@Target( { ElementType.TYPE })
+@Target({ ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 @Documented

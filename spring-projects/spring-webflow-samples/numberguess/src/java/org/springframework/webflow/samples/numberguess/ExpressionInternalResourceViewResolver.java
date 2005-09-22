836ba773d3963ac0d1f7d7fe25c46b0222d5c230
cvs2git <admin@example.com>
2005-09-22 22:57:01 +0000

    This commit was manufactured by cvs2git to create branch 'branch-1-2'.

--- spring-projects/spring-webflow-samples/numberguess/src/java/org/springframework/webflow/samples/numberguess/ExpressionInternalResourceViewResolver.java
+++
@@ -1,7 +0,0 @@
-package org.springframework.webflow.samples.numberguess;
-
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
-public class ExpressionInternalResourceViewResolver extends InternalResourceViewResolver {
-
-}

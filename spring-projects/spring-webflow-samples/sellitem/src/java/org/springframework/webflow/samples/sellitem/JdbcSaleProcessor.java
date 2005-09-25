0fffde77cf74ac9bed6f8a6e9ef2c9fd7a2cea11
kdonald <>
2005-09-25 05:40:58 +0000

    *** empty log message ***

---
+++ spring-projects/spring-webflow-samples/sellitem/src/java/org/springframework/webflow/samples/sellitem/JdbcSaleProcessor.java
@@ -0,0 +1,10 @@
+package org.springframework.webflow.samples.sellitem;
+
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+
+public class JdbcSaleProcessor extends JdbcDaoSupport implements SaleProcessor {
+	public void process(Sale sale) {
+		getJdbcTemplate().update("insert into SALES values (?, ?, ?, ?)",
+				new Object[] { sale.getPrice(), sale.getItemCount(), sale.getCategory(), sale.getShippingType() });
+	}
+}

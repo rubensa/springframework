181acd93ef42fe3dd08bf7a32b35091849d9d5cd
kdonald <>
2005-09-25 05:58:55 +0000

    polishing

--- spring-projects/spring-webflow-samples/sellitem/src/test/org/springframework/webflow/samples/sellitem/SellItemFlowExecutionTests.java
+++ spring-projects/spring-webflow-samples/sellitem/src/test/org/springframework/webflow/samples/sellitem/SellItemFlowExecutionTests.java
@@ -1,5 +1,9 @@
 package org.springframework.webflow.samples.sellitem;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.webflow.ViewDescriptor;
 import org.springframework.webflow.test.AbstractFlowExecutionTests;
 
 public class SellItemFlowExecutionTests extends AbstractFlowExecutionTests {
@@ -13,6 +17,43 @@ public class SellItemFlowExecutionTests extends AbstractFlowExecutionTests {
 	}
 
 	public void testStartFlow() {
-		startFlow();
+		ViewDescriptor nextView = startFlow();
+		assertViewNameEquals("priceAndItemCountForm", nextView);
+	}
+	
+	public void testSubmitPriceAndItemCount() {
+		testStartFlow();
+		Map parameters = new HashMap(2);
+		parameters.put("itemCount", 4);
+		parameters.put("price", 25);
+		ViewDescriptor nextView = signalEvent(event("submit", parameters));
+		assertViewNameEquals("categoryForm", nextView);
+	}
+	
+	public void testSubmitCategoryForm() {
+		testSubmitPriceAndItemCount();
+		Map parameters = new HashMap(1);
+		parameters.put("category", "A");
+		ViewDescriptor nextView = signalEvent(event("submit", parameters));
+		assertViewNameEquals("showCostOverview", nextView);
+		assertFlowExecutionEnded();
+	}
+
+	public void testSubmitCategoryFormWithShipping() {
+		testSubmitPriceAndItemCount();
+		Map parameters = new HashMap(1);
+		parameters.put("category", "A");
+		parameters.put("shipping", "true");
+		ViewDescriptor nextView = signalEvent(event("submit", parameters));
+		assertViewNameEquals("shippingDetailsForm", nextView);
+	}
+
+	public void testSubmitShippingDetailsForm() {
+		testSubmitCategoryFormWithShipping();
+		Map parameters = new HashMap(1);
+		parameters.put("shippingType", "E");
+		ViewDescriptor nextView = signalEvent(event("submit", parameters));
+		assertViewNameEquals("showCostOverview", nextView);
+		assertFlowExecutionEnded();
 	}
 }

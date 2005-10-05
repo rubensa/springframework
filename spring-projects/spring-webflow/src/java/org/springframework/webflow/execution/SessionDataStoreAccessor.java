62a137e7f283d0d7da14db0662ece0e4e957047b
kdonald <>
2005-10-05 03:52:04 +0000

    *** empty log message ***

--- spring-projects/spring-webflow/src/java/org/springframework/webflow/execution/SessionDataStoreAccessor.java
+++ spring-projects/spring-webflow/src/java/org/springframework/webflow/execution/SessionDataStoreAccessor.java
@@ -4,6 +4,10 @@ import org.springframework.binding.MutableAttributeSource;
 import org.springframework.binding.support.MapAttributeSource;
 import org.springframework.webflow.Event;
 
+/**
+ * A data source accessor that accesses the external context session map.
+ * @author Keith Donald
+ */
 public class SessionDataStoreAccessor implements DataStoreAccessor {
 	public MutableAttributeSource getDataStore(Event sourceEvent) {
 		return new MapAttributeSource(((ExternalEvent)sourceEvent).getSessionMap());

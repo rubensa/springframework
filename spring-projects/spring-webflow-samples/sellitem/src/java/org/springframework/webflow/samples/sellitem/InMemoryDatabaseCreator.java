56bc11ac12d3bac5a1844de9a96af8f52272158f
kdonald <>
2005-10-13 04:21:21 +0000

    *** empty log message ***

--- spring-projects/spring-webflow-samples/sellitem/src/java/org/springframework/webflow/samples/sellitem/InMemoryDatabaseCreator.java
+++ spring-projects/spring-webflow-samples/sellitem/src/java/org/springframework/webflow/samples/sellitem/InMemoryDatabaseCreator.java
@@ -6,7 +6,7 @@ public class InMemoryDatabaseCreator extends JdbcDaoSupport {
 
 	@Override
 	protected void initDao() throws Exception {
-		String createSales = "create table SALES (ID int not null identity primary key, ITEM_COUNT int not null, PRICE double NOT NULL, category VARCHAR(1) NOT NULL, SHIPPING_TYPE varchar(1))";
+		String createSales = "create table T_SALES (ID int not null identity primary key, ITEM_COUNT int not null, PRICE double NOT NULL, category VARCHAR(1) NOT NULL, SHIPPING_TYPE varchar(1))";
 		getJdbcTemplate().execute(createSales);
 	}
 

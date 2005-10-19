58225bee6a4304d7e2756683d13170f1bcbc085a
kdonald <>
2005-10-19 23:31:06 +0000

    *** empty log message ***

--- spring-projects/spring-webflow/src/java/org/springframework/webflow/RequestNotInTransactionException.java
+++ spring-projects/spring-webflow/src/java/org/springframework/webflow/RequestNotInTransactionException.java
@@ -2,7 +2,18 @@ package org.springframework.webflow;
 
 import org.springframework.core.NestedRuntimeException;
 
+/**
+ * An exception indicating a request to resume a Flow execution did not occur in
+ * the context of the ongoing "application transaction" associated with that
+ * Flow execution.
+ * 
+ * @author Keith Donald
+ */
 public class RequestNotInTransactionException extends NestedRuntimeException {
+
+	/**
+	 * Creates a default instance.
+	 */
 	public RequestNotInTransactionException() {
 		super("The request is not executing in the context of an application transaction");
 	}

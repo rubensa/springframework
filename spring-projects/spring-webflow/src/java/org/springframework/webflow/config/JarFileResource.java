5030384649e428b4aa34aaf3ce72e405a0366fd4
kdonald <>
2005-10-20 14:39:40 +0000

    *** empty log message ***

--- spring-projects/spring-webflow/src/java/org/springframework/webflow/config/JarFileResource.java
+++ spring-projects/spring-webflow/src/java/org/springframework/webflow/config/JarFileResource.java
@@ -13,19 +13,37 @@ import org.springframework.core.io.AbstractResource;
  */
 public class JarFileResource extends AbstractResource {
 
+	/**
+	 * The Jar File where the resource resides
+	 */
 	private JarFile jarFile;
 
+	/**
+	 * The entry in the jar file representing this resource.
+	 */
 	private ZipEntry entry;
 
+	/**
+	 * Constructs a new jar file resource.
+	 * 
+	 * @param jarFile the jar file
+	 * @param entry the jar file entry
+	 */
 	public JarFileResource(JarFile jarFile, ZipEntry entry) {
 		this.jarFile = jarFile;
 		this.entry = entry;
 	}
 
+	/**
+	 * Returns the JAR file.
+	 */
 	public JarFile getJarFile() {
 		return jarFile;
 	}
 
+	/**
+	 * Returns the JAR file zip entry representing this resource.
+	 */
 	public ZipEntry getEntry() {
 		return entry;
 	}

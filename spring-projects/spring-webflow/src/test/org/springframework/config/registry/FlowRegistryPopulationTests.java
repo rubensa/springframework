3219cdf35a4362309e093d08238153e8240058f1
kdonald <>
2005-10-21 15:03:55 +0000

    registry tests

---
+++ spring-projects/spring-webflow/src/test/org/springframework/config/registry/FlowRegistryPopulationTests.java
@@ -0,0 +1,36 @@
+package org.springframework.config.registry;
+
+import junit.framework.TestCase;
+
+import org.springframework.webflow.config.AbstractFlowBuilder;
+import org.springframework.webflow.config.FlowBuilder;
+import org.springframework.webflow.config.FlowBuilderException;
+import org.springframework.webflow.config.registry.FlowRegistrarImpl;
+import org.springframework.webflow.config.registry.FlowRegistryImpl;
+
+public class FlowRegistryPopulationTests extends TestCase {
+	public void testDefaultPopulation() {
+		FlowRegistryImpl registry = new FlowRegistryImpl();
+		FlowBuilder[] builders = new FlowBuilder[] {
+				new AbstractFlowBuilder() {
+					protected String flowId() {
+						return "flow1";
+					}
+					public void buildStates() throws FlowBuilderException {
+						addEndState("end");
+					}
+				},
+				new AbstractFlowBuilder() {
+					protected String flowId() {
+						return "flow2";
+					}
+					public void buildStates() throws FlowBuilderException {
+						addEndState("end");
+					}
+				}
+		};
+		FlowRegistrarImpl registrar = new FlowRegistrarImpl(builders);
+		registrar.registerFlowDefinitions(registry);
+		assertEquals("Wrong registry definition count", 2, registry.getFlowDefinitionCount());
+	}
+}

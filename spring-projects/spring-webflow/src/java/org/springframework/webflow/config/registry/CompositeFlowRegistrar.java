e4fa43fd3ce28b687c151b74e4caa4572842249d
kdonald <>
2005-10-21 14:00:57 +0000

    composite makes more sense here

---
+++ spring-projects/spring-webflow/src/java/org/springframework/webflow/config/registry/CompositeFlowRegistrar.java
@@ -0,0 +1,31 @@
+package org.springframework.webflow.config.registry;
+
+import org.springframework.util.Assert;
+
+/**
+ * A flow registrar that applies an ordered list of Flow Registrar
+ * implementations as part of a chain.
+ * @author Keith Donald
+ */
+public class CompositeFlowRegistrar implements FlowRegistrar {
+
+	/**
+	 * The flow registrar chain.
+	 */
+	private FlowRegistrar[] registrars;
+
+	/**
+	 * Creates a chained flow definition registrar.
+	 * @param registrars the chain
+	 */
+	public CompositeFlowRegistrar(FlowRegistrar[] registrars) {
+		Assert.notEmpty(registrars, "There must be at least 1 Flow Registrar in the chain");
+		this.registrars = registrars;
+	}
+
+	public void registerFlowDefinitions(ConfigurableFlowRegistry registry) {
+		for (int i = 0; i < registrars.length; i++) {
+			registrars[i].registerFlowDefinitions(registry);
+		}
+	}
+}

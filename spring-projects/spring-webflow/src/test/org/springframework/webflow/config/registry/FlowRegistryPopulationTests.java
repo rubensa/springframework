48b2ed07b9aa3fc5ae07cd3ee4d3f8d8f059bde6
kdonald <>
2005-10-21 15:49:02 +0000

    *** empty log message ***

--- spring-projects/spring-webflow/src/test/org/springframework/webflow/config/registry/FlowRegistryPopulationTests.java
+++ spring-projects/spring-webflow/src/test/org/springframework/webflow/config/registry/FlowRegistryPopulationTests.java
@@ -1,5 +1,7 @@
 package org.springframework.webflow.config.registry;
 
+import java.io.File;
+
 import junit.framework.TestCase;
 
 import org.springframework.core.io.FileSystemResource;
@@ -8,9 +10,6 @@ import org.springframework.webflow.config.AbstractFlowBuilder;
 import org.springframework.webflow.config.FlowArtifactLocatorAdapter;
 import org.springframework.webflow.config.FlowBuilder;
 import org.springframework.webflow.config.FlowBuilderException;
-import org.springframework.webflow.config.registry.FlowRegistrarImpl;
-import org.springframework.webflow.config.registry.FlowRegistryImpl;
-import org.springframework.webflow.config.registry.XmlFlowRegistrar;
 
 public class FlowRegistryPopulationTests extends TestCase {
 	public void testDefaultPopulation() {
@@ -42,9 +41,10 @@ public class FlowRegistryPopulationTests extends TestCase {
 	
 	public void testXmlPopulation() {
 		FlowRegistryImpl registry = new FlowRegistryImpl();
+		File parent = new File("src/test/org/springframework/webflow/config/registry");
 		Resource[] locations = new Resource[] {
-				new FileSystemResource("flow1.xml"),
-				new FileSystemResource("flow2.xml")
+				new FileSystemResource(new File(parent, "flow1.xml")),
+				new FileSystemResource(new File(parent, "flow2.xml"))
 		};
 		XmlFlowRegistrar registrar = new XmlFlowRegistrar(new FlowArtifactLocatorAdapter(), locations);
 		registrar.registerFlowDefinitions(registry);

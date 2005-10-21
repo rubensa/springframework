43beb65db5117e296caf4c893755b3fc247952a8
kdonald <>
2005-10-21 14:57:40 +0000

    *** empty log message ***

--- spring-projects/spring-webflow/src/java/org/springframework/webflow/config/registry/FlowRegistrarImpl.java
+++ spring-projects/spring-webflow/src/java/org/springframework/webflow/config/registry/FlowRegistrarImpl.java
@@ -6,8 +6,8 @@ import org.springframework.webflow.config.FlowAssembler;
 import org.springframework.webflow.config.FlowBuilder;
 
 /**
- * A Flow definition registrar that registers all flow definitions built by a
- * configured set of Flow builders.
+ * A default flow registrar implementation that registers flow definitions built
+ * by a configured set of Flow builders.
  * @author Keith Donald
  */
 public class FlowRegistrarImpl implements FlowRegistrar {
@@ -18,7 +18,22 @@ public class FlowRegistrarImpl implements FlowRegistrar {
 	private FlowBuilder[] flowBuilders;
 
 	/**
-	 * Sets the flow builders that will build the Flow definitions to register.
+	 * Creates a Flow registrar with an initially empty builder list.
+	 */
+	public FlowRegistrarImpl() {
+
+	}
+
+	/**
+	 * Creates a Flow registrar registering Flows built by the provided builder
+	 * list.
+	 */
+	public FlowRegistrarImpl(FlowBuilder[] flowBuilders) {
+		this.flowBuilders = flowBuilders;
+	}
+
+	/**
+	 * Sets the builders that will build the Flow definitions to be registered.
 	 */
 	public void setFlowBuilders(FlowBuilder[] flowBuilders) {
 		this.flowBuilders = flowBuilders;

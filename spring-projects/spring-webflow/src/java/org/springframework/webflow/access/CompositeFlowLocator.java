b3d688bef0faff72d18b6fa7e4e9affa194b58d8
kdonald <>
2005-10-23 03:32:53 +0000

    polishing

--- spring-projects/spring-webflow/src/java/org/springframework/webflow/access/CompositeFlowLocator.java
+++ spring-projects/spring-webflow/src/java/org/springframework/webflow/access/CompositeFlowLocator.java
@@ -6,29 +6,29 @@ import org.springframework.webflow.Flow;
 /**
  * Delegates a request to locate a Flow definition by ID to a ordered chain of
  * FlowLocators. Iterates through the list until the Flow definition is found or
- * the chain is exhausted.
+ * the chain is exhausted and a NoSuchFlowDefinition exception is thrown.
  * @author Keith Donald
  */
-public class ChainedFlowLocator implements FlowLocator {
+public class CompositeFlowLocator implements FlowLocator {
 
 	/**
 	 * The FlowLocator chain.
 	 */
-	private FlowLocator[] locatorChain;
+	private FlowLocator[] locators;
 
 	/**
 	 * Creates a chained flow locator made from the list of specified locators.
-	 * @param locatorChain The FlowLocator list
+	 * @param locators The FlowLocator list
 	 */
-	public ChainedFlowLocator(FlowLocator[] locatorChain) {
-		Assert.notEmpty(locatorChain, "The flow locator chain cannot be empty");
-		this.locatorChain = locatorChain;
+	public CompositeFlowLocator(FlowLocator[] locators) {
+		Assert.notEmpty(locators, "The flow locator chain cannot be empty");
+		this.locators = locators;
 	}
 
 	public Flow getFlow(String id) throws FlowArtifactLookupException {
-		for (int i = 0; i < locatorChain.length; i++) {
+		for (int i = 0; i < locators.length; i++) {
 			try {
-				return locatorChain[i].getFlow(id);
+				return locators[i].getFlow(id);
 			}
 			catch (FlowArtifactLookupException e) {
 

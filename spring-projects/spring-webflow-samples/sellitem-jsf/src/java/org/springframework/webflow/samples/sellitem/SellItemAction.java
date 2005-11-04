27272792a20a8d41a730f41b5487ecf8002d687c
klr8 <>
2005-11-04 13:09:18 +0000

    Cleanup so that stuff compiles again.

--- spring-projects/spring-webflow-samples/sellitem-jsf/src/java/org/springframework/webflow/samples/sellitem/SellItemAction.java
+++ spring-projects/spring-webflow-samples/sellitem-jsf/src/java/org/springframework/webflow/samples/sellitem/SellItemAction.java
@@ -2,15 +2,14 @@ package org.springframework.webflow.samples.sellitem;
 
 import org.springframework.webflow.Event;
 import org.springframework.webflow.RequestContext;
-import org.springframework.webflow.Scope;
 import org.springframework.webflow.action.AbstractAction;
 
 public class SellItemAction extends AbstractAction {
 
     protected Event doExecute(RequestContext context) throws Exception {
-        Scope flow = context.getFlowScope();
+        //Scope flow = context.getFlowScope();
         
-        Sale sale = (Sale) flow.get("sale");
+        //Sale sale = (Sale) flow.get("sale");
         
         // this is where we can validate
         

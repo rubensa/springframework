2194451a5d8d92235bbfb0c88ad6ebe4d4996592
klr8 <>
2005-11-06 13:25:53 +0000

    JavaDoc code review.

--- spring-projects/spring-webflow/src/java/org/springframework/webflow/StateException.java
+++ spring-projects/spring-webflow/src/java/org/springframework/webflow/StateException.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2002-2005 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.webflow;
 
 import org.springframework.core.NestedRuntimeException;
@@ -5,6 +20,8 @@ import org.springframework.core.NestedRuntimeException;
 /**
  * Core base class for exceptions that occur during in a flow state. Provides a
  * reference to the State definition where the exception occured.
+ * 
+ * @see org.springframework.webflow.State
  * 
  * @author Keith Donald
  */
@@ -17,7 +34,6 @@ public class StateException extends NestedRuntimeException {
 
 	/**
 	 * Creates a new state exception.
-	 * 
 	 * @param state the state where the exception occured
 	 * @param message a descriptive message
 	 */
@@ -28,7 +44,6 @@ public class StateException extends NestedRuntimeException {
 
 	/**
 	 * Creates a new state exception.
-	 * 
 	 * @param state the state where the exception occured
 	 * @param message a descriptive message
 	 * @param cause the root cause

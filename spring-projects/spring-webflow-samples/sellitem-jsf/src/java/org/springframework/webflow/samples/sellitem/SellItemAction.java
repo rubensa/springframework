02eef24a76a1aad10f61ee5235098241c0667c1b
kdonald <>
2005-11-12 18:39:46 +0000

    try this for now

--- spring-projects/spring-webflow-samples/sellitem-jsf/src/java/org/springframework/webflow/samples/sellitem/SellItemAction.java
+++ spring-projects/spring-webflow-samples/sellitem-jsf/src/java/org/springframework/webflow/samples/sellitem/SellItemAction.java
@@ -2,19 +2,15 @@ package org.springframework.webflow.samples.sellitem;
 
 import org.springframework.webflow.Event;
 import org.springframework.webflow.RequestContext;
-import org.springframework.webflow.Scope;
 import org.springframework.webflow.action.AbstractAction;
 
 public class SellItemAction extends AbstractAction {
-
+	
 	// this does nothing. We're just showing how in the JSF integration, while an action is
-    // not needed for binding, you can still add in an action to do somethingif you need to
+    // not needed for binding, you can still add in an action to do something if you need to
 	protected Event doExecute(RequestContext context) throws Exception {
-        Scope flow = context.getFlowScope();
-        
-        Sale sale = (Sale) flow.get("sale");
+        Sale sale = (Sale)context.getFlowScope().getAttribute("sale");
         sale.getAmount();
-        
         return success();
     }
 

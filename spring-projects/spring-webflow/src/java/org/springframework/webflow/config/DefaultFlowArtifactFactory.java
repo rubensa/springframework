b0c6dcea09da1394b107fed98aa3d81aaa80066a
kdonald <>
2005-11-17 08:08:49 +0000

    polish

--- spring-projects/spring-webflow/src/java/org/springframework/webflow/config/DefaultFlowArtifactFactory.java
+++ spring-projects/spring-webflow/src/java/org/springframework/webflow/config/DefaultFlowArtifactFactory.java
@@ -1,14 +1,22 @@
 package org.springframework.webflow.config;
 
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.util.Assert;
+import org.springframework.webflow.Action;
+import org.springframework.webflow.FlowArtifactException;
+import org.springframework.webflow.FlowAttributeMapper;
+import org.springframework.webflow.NoSuchFlowArtifactException;
+import org.springframework.webflow.StateExceptionHandler;
+import org.springframework.webflow.TransitionCriteria;
+import org.springframework.webflow.ViewSelector;
 
 /**
  * A flow artifact locator that pulls its artifacts from a standard Spring
  * BeanFactory.
  * @author Keith Donald
  */
-public class DefaultFlowArtifactFactory extends AbstractFlowArtifactFactory {
+public class DefaultFlowArtifactFactory extends FlowArtifactFactoryAdapter {
 
 	/**
 	 * The Spring bean factory.
@@ -25,6 +33,35 @@ public class DefaultFlowArtifactFactory extends AbstractFlowArtifactFactory {
 		super(subflowLocator);
 		Assert.notNull(beanFactory, "The beanFactory to retrieve flow artifacts is required");
 		this.beanFactory = beanFactory;
+	}
+
+	public Action getAction(String id) throws FlowArtifactException {
+		return toAction(getArtifact(id, Action.class));
+	}
+
+	public FlowAttributeMapper getAttributeMapper(String id) throws FlowArtifactException {
+		return (FlowAttributeMapper)getArtifact(id, FlowAttributeMapper.class);
+	}
+
+	public TransitionCriteria getTransitionCriteria(String id) throws FlowArtifactException {
+		return (TransitionCriteria)getArtifact(id, TransitionCriteria.class);
+	}
+
+	public ViewSelector getViewSelector(String id) throws FlowArtifactException {
+		return (ViewSelector)getArtifact(id, ViewSelector.class);
+	}
+
+	public StateExceptionHandler getExceptionHandler(String id) throws FlowArtifactException {
+		return (StateExceptionHandler)getArtifact(id, StateExceptionHandler.class);
+	}
+
+	private Object getArtifact(String id, Class artifactType) {
+		try {
+			return doGetArtifact(id, artifactType);
+		}
+		catch (NoSuchBeanDefinitionException e) {
+			throw new NoSuchFlowArtifactException(artifactType, id, e);
+		}
 	}
 
 	protected Object doGetArtifact(String id, Class artifactType) {

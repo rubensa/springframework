ad48d8a3cdda14446e7f556f23ff2ff35898ecb0
kdonald <>
2005-11-24 17:15:24 +0000

    doco

--- spring-projects/spring-webflow/src/java/org/springframework/webflow/test/MockExternalContext.java
+++ spring-projects/spring-webflow/src/java/org/springframework/webflow/test/MockExternalContext.java
@@ -22,15 +22,27 @@ public class MockExternalContext implements ExternalContext {
 	
 	private Map applicationMap = new TreeMap();
 	
+	/**
+	 * Creates a mock external context with an empty request parameter map.
+	 */
 	public MockExternalContext() {
 		
 	}
 	
+	/**
+	 * Creates a mock external context with a single parameter in the request parameter map.
+	 * @param singleRequestParameterName the parameter name
+	 * @param singleRequestParameterValue the parameter value
+	 */	
 	public MockExternalContext(String singleRequestParameterName, Object singleRequestParameterValue) {
 		this.requestParameterMap = new HashMap(1);
 		this.requestParameterMap.put(singleRequestParameterName, singleRequestParameterValue);
 	}
 	
+	/**
+	 * Creates a mock external context with the specified parameters in the request parameter map.
+	 * @param requestParameterMap the request parameter map
+	 */
 	public MockExternalContext(Map requestParameterMap) {
 		this.requestParameterMap = requestParameterMap;
 	}

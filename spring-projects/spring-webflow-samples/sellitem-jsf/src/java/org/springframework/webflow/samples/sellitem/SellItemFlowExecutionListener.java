39a853f312e26034a912afc3632d9288fe0c409f
kdonald <>
2005-11-24 10:14:21 +0000

    oops

--- spring-projects/spring-webflow-samples/sellitem-jsf/src/java/org/springframework/webflow/samples/sellitem/SellItemFlowExecutionListener.java
+++ spring-projects/spring-webflow-samples/sellitem-jsf/src/java/org/springframework/webflow/samples/sellitem/SellItemFlowExecutionListener.java
@@ -8,12 +8,13 @@ import org.springframework.webflow.State;
 import org.springframework.webflow.execution.EnterStateVetoException;
 import org.springframework.webflow.execution.FlowExecutionListenerAdapter;
 import org.springframework.webflow.execution.servlet.ServletEvent;
+import org.springframework.webflow.execution.servlet.ServletExternalContext;
 
 public class SellItemFlowExecutionListener extends FlowExecutionListenerAdapter {
 	public void stateEntering(RequestContext context, State nextState) throws EnterStateVetoException {
 		String role = (String)nextState.getProperty("role");
 		if (StringUtils.hasText(role)) {
-			HttpServletRequest request = ((ServletEvent)context.getSourceEvent()).getRequest();
+			HttpServletRequest request = ((ServletExternalContext)context.getExternalContext()).getRequest();
 			if (!request.isUserInRole(role)) {
 				throw new EnterStateVetoException(context.getFlowExecutionContext().getCurrentState(), nextState,
 						"State requires role '" + role + "', but the authenticated user doesn't have it!");

11ecf0e6a12b2644d7ded29e338e3c3f191ab7e4
klr8 <>
2005-11-25 19:07:28 +0000

    Fixed JavaDoc typo and added license header.

--- spring-projects/spring-webflow/src/java/org/springframework/webflow/config/BaseFlowBuilder.java
+++ spring-projects/spring-webflow/src/java/org/springframework/webflow/config/BaseFlowBuilder.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2002-2005 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.webflow.config;
 
 import org.apache.commons.logging.Log;
@@ -16,7 +31,7 @@ import org.springframework.webflow.support.TextToViewSelector;
 /**
  * Abstract base implementation of a flow builder defining common functionality
  * needed by most concrete flow builder implementations. All flow related artifacts
- * are expected to be defined in the bean factory defiing this flow builder.
+ * are expected to be defined in the bean factory defining this flow builder.
  * Subclasses can use a {@link org.springframework.webflow.config.FlowArtifactFactory}
  * to easily access that bean factory.
  * 

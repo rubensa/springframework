d181d40f286129bdfddf77aeae895fa74313f1be
kdonald <>
2005-11-26 19:32:29 +0000

    removed hardcoded dep on swf project

---
+++ spring-projects/spring-webflow-samples/phonebook-portlet/src/java/org/springframework/webflow/samples/phonebook/web/FlowRegistryFactoryBean.java
@@ -0,0 +1,9 @@
+package org.springframework.webflow.samples.phonebook.web;
+
+import org.springframework.webflow.config.registry.AbstractFlowRegistryFactoryBean;
+import org.springframework.webflow.config.registry.FlowRegistry;
+
+public class FlowRegistryFactoryBean extends AbstractFlowRegistryFactoryBean {
+	protected void doPopulate(FlowRegistry registry) {
+	}
+}

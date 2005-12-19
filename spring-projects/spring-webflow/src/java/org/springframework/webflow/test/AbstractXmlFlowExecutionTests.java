acbf62a9123aff61b26b57aa57b6f2339fd1b169
kdonald <>
2005-12-19 04:33:04 +0000

    adding javadoc

--- spring-projects/spring-webflow/src/java/org/springframework/webflow/test/AbstractXmlFlowExecutionTests.java
+++ spring-projects/spring-webflow/src/java/org/springframework/webflow/test/AbstractXmlFlowExecutionTests.java
@@ -4,8 +4,41 @@ import org.springframework.core.io.Resource;
 import org.springframework.webflow.registry.XmlFlowRegistrar;
 
 /**
- * Base class for flow integration tests that verify a XML flow definition executes
- * as expected.
+ * Base class for flow integration tests that verify a XML flow definition
+ * executes as expected. Sample usage:
+ * 
+ * <pre>
+ *  public class SearchFlowExecutionTests extends AbstractXmlFlowExecutionTests {
+ * 
+ *      protected String flowId() {
+ *          return &quot;search&quot;;
+ *      }
+ *  
+ *      protected Resource getFlowLocation() {
+ *          File flowDir = new File("src/webapp/WEB-INF");
+ *          return new FileSystemResource(new File(flowDir, "search.xml"));
+ *      }
+ * 
+ *      protected String[] getConfigLocations() {
+ *          return new String[] { &quot;classpath:example/applicationContext.xml };
+ *      }
+ * 
+ *      public void testStartFlow() {
+ *          startFlow();
+ *          assertCurrentStateEquals(&quot;displaySearchCriteria&quot;);
+ *      }
+ * 
+ *      public void testDisplayCriteriaSubmitSuccess() {
+ *          startFlow();
+ *          Map parameters = new HashMap();
+ *          parameters.put(&quot;firstName&quot;, &quot;Keith&quot;);
+ *          parameters.put(&quot;lastName&quot;, &quot;Donald&quot;);
+ *          ViewSelection view = signalEvent(&quot;search&quot;, parameters);
+ *          assertCurrentStateEquals(&quot;displayResults&quot;);
+ *          assertModelAttributeCollectionSize(1, &quot;results&quot;, view);
+ *      }
+ *  }
+ * </pre>
  * 
  * @author Keith Donald
  */
@@ -21,7 +54,7 @@ public abstract class AbstractXmlFlowExecutionTests extends AbstractFlowRegistry
 		registrar.addFlowLocations(getSubflowLocations());
 		registrar.registerFlows(getFlowRegistry(), getFlowArtifactFactory());
 	}
-	
+
 	/**
 	 * Returns the resource pointing to the XML-based flow definition needed by
 	 * this flow execution test: subclasses must override.
@@ -35,7 +68,7 @@ public abstract class AbstractXmlFlowExecutionTests extends AbstractFlowRegistry
 
 	/**
 	 * Returns the array of resources pointing to the XML-based subflow
-	 * definitions needed by this flow execution test.  Optional.
+	 * definitions needed by this flow execution test. Optional.
 	 * <p>
 	 * Flow definitions stored in the returned resouce array are automatically
 	 * added to this test's FlowRegistry by the {@link #populateFlowRegistry}

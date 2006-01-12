24f629e13d1ececfa248f9e59a67365f9e77db41
kdonald <>
2006-01-12 18:21:21 +0000

    polishing

--- spring-projects/spring-webflow/src/java/org/springframework/webflow/manager/struts/ActionFormAdapter.java
+++ spring-projects/spring-webflow/src/java/org/springframework/webflow/manager/struts/ActionFormAdapter.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2002-2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.webflow.manager.struts;
 
 import org.springframework.web.struts.SpringBindingActionForm;
@@ -10,7 +25,7 @@ import org.springframework.webflow.execution.FlowExecutionListenerAdapter;
  * supporting POJO-based data binding in request scope under a well-defined
  * name and adapts it to the Struts ActionForm model.
  */
-class ActionFormAdapter extends FlowExecutionListenerAdapter {
+public class ActionFormAdapter extends FlowExecutionListenerAdapter {
 	public void requestProcessed(RequestContext context) {
 		if (context.getFlowExecutionContext().isActive()) {
 			StrutsExternalContext strutsContext = (StrutsExternalContext)context.getExternalContext();

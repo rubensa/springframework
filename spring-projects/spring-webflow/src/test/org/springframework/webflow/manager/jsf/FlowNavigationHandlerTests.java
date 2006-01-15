887cebcad3982c5e8069a6079227f740674658e5
kdonald <>
2006-01-15 05:24:19 +0000

    javadoc

--- spring-projects/spring-webflow/src/test/org/springframework/webflow/manager/jsf/FlowNavigationHandlerTests.java
+++ spring-projects/spring-webflow/src/test/org/springframework/webflow/manager/jsf/FlowNavigationHandlerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,64 +15,130 @@
  */
 package org.springframework.webflow.manager.jsf;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.security.Principal;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Set;
-
-import javax.faces.application.Application;
-import javax.faces.application.FacesMessage;
 import javax.faces.application.NavigationHandler;
-import javax.faces.application.FacesMessage.Severity;
-import javax.faces.component.UIViewRoot;
-import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.faces.context.ResponseStream;
-import javax.faces.context.ResponseWriter;
-import javax.faces.render.RenderKit;
 
 import junit.framework.TestCase;
 
+import org.easymock.MockControl;
+import org.springframework.webflow.Flow;
+import org.springframework.webflow.ViewSelection;
+import org.springframework.webflow.execution.FlowExecution;
+import org.springframework.webflow.execution.FlowLocator;
+
 /**
- * Test case for the FlowNavigationHandler class.
+ * Unit tests for the FlowNavigationHandler class.
  * 
  * @author Ulrik Sandberg
  */
 public class FlowNavigationHandlerTests extends TestCase {
 
-	private FlowNavigationHandler tested;
-	
-	private FacesContext facesContext;
+	private MockControl flowExecutionControl;
+
+	private FlowExecution flowExecutionMock;
+
+	private MockControl flowLocatorControl;
+
+	private FlowLocator flowLocatorMock;
+
+	private MockFacesContext mockFacesContext;
+
+	private MockJsfExternalContext mockJsfExternalContext;
 
 	private MyNavigationHandler navigationHandler;
 
+	private FlowExecutionHolder flowExecutionHolder;
+
+	private boolean renderViewCalled;
+
+	private FlowNavigationHandler tested;
+
+	private ViewSelection viewSelection;
+
 	protected void setUp() throws Exception {
 		super.setUp();
+		flowExecutionControl = MockControl.createControl(FlowExecution.class);
+		flowExecutionMock = (FlowExecution) flowExecutionControl.getMock();
+
+		flowLocatorControl = MockControl.createControl(FlowLocator.class);
+		flowLocatorMock = (FlowLocator) flowLocatorControl.getMock();
+
+		viewSelection = new ViewSelection("SomeView");
 		navigationHandler = new MyNavigationHandler();
-		facesContext = new MyFacesContext();
+		mockFacesContext = new MockFacesContext();
+		mockJsfExternalContext = new MockJsfExternalContext();
+		mockFacesContext.setExternalContext(mockJsfExternalContext);
+		flowExecutionHolder = new FlowExecutionHolder(flowExecutionMock);
+		tested = new FlowNavigationHandler(navigationHandler) {
+			public void renderView(ViewSelection selectedView, FacesContext facesContext) {
+				renderViewCalled = true;
+			}
+
+			protected FlowLocator getFlowLocator(FacesContext context) {
+				return flowLocatorMock;
+			}
+
+			protected FlowExecution createFlowExecution(Flow flow, FacesContext context) {
+				return flowExecutionMock;
+			}
+		};
 	}
 
-	public void testHandleNavigationNoFlowLaunchNoFlowExecutionParticipation() {
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		flowExecutionControl = null;
+		flowExecutionMock = null;
+
+		flowLocatorControl = null;
+		flowLocatorMock = null;
+
+		tested = null;
+	}
+
+	protected void replay() {
+		flowExecutionControl.replay();
+		flowLocatorControl.replay();
+	}
+
+	protected void verify() {
+		flowExecutionControl.verify();
+		flowLocatorControl.verify();
+	}
+
+	public void testHandleNavigationNoHolderNoFlowId() {
+		replay();
+
 		// perform test
-		tested.handleNavigation(facesContext, "FromAction", "OutCome");
+		tested.handleNavigation(mockFacesContext, "FromAction", "OutCome");
+
+		verify();
 		assertTrue("delegate not called", navigationHandler.handled);
 	}
 
-	public void testHandleNavigationFlowLaunch() {
+	public void testHandleNavigationHasHolder() {
+		mockJsfExternalContext.getRequestMap().put(FlowExecutionHolder.class.getName(), flowExecutionHolder);
+		flowExecutionControl.expectAndReturn(flowExecutionMock.signalEvent("OutCome", new JsfExternalContext(mockFacesContext,
+				"FromAction", "OutCome")), viewSelection);
+		replay();
+
 		// perform test
-		tested.handleNavigation(facesContext, "FromAction", "OutCome");
+		tested.handleNavigation(mockFacesContext, "FromAction", "OutCome");
+
+		verify();
+		assertTrue("renderView not called", renderViewCalled);
 		assertFalse("delegate called", navigationHandler.handled);
 	}
 
-	public void testHandleNavigationFlowExecutionParticipation() {
+	public void testHandleNavigationHasNoHolder() {
+		flowLocatorControl.expectAndReturn(flowLocatorMock.getFlow("OutCome"), null);
+		flowExecutionControl.expectAndReturn(flowExecutionMock.start(new JsfExternalContext(mockFacesContext, "FromAction",
+				"flowId:OutCome")), viewSelection);
+		replay();
+
 		// perform test
-		tested.handleNavigation(facesContext, "FromAction", "OutCome");
+		tested.handleNavigation(mockFacesContext, "FromAction", "flowId:OutCome");
+
+		verify();
 		assertFalse("delegate called", navigationHandler.handled);
 	}
 
@@ -81,214 +147,6 @@ public class FlowNavigationHandlerTests extends TestCase {
 
 		public void handleNavigation(FacesContext facesContext, String fromAction, String outcome) {
 			handled = true;
-		}
-	}
-
-	private static class MyFacesContext extends FacesContext {
-		public Application getApplication() {
-			return null;
-		}
-
-		public Iterator getClientIdsWithMessages() {
-			return null;
-		}
-
-		public ExternalContext getExternalContext() {
-			return new FlowNavigationHandlerTests.MyExternalContext();
-		}
-
-		public Severity getMaximumSeverity() {
-			return null;
-		}
-
-		public Iterator getMessages() {
-			return null;
-		}
-
-		public Iterator getMessages(String arg0) {
-			return null;
-		}
-
-		public RenderKit getRenderKit() {
-			return null;
-		}
-
-		public boolean getRenderResponse() {
-			return false;
-		}
-
-		public boolean getResponseComplete() {
-			return false;
-		}
-
-		public ResponseStream getResponseStream() {
-			return null;
-		}
-
-		public void setResponseStream(ResponseStream arg0) {
-
-		}
-
-		public ResponseWriter getResponseWriter() {
-			return null;
-		}
-
-		public void setResponseWriter(ResponseWriter arg0) {
-		}
-
-		public UIViewRoot getViewRoot() {
-			UIViewRoot viewRoot = new UIViewRoot();
-			viewRoot.setViewId("ViewId");
-			return viewRoot;
-		}
-
-		public void setViewRoot(UIViewRoot arg0) {
-		}
-
-		public void addMessage(String arg0, FacesMessage arg1) {
-		}
-
-		public void release() {
-		}
-
-		public void renderResponse() {
-		}
-
-		public void responseComplete() {
-		}
-	}
-
-	private static class MyExternalContext extends ExternalContext {
-		public void dispatch(String arg0) throws IOException {
-		}
-
-		public String encodeActionURL(String arg0) {
-			return null;
-		}
-
-		public String encodeNamespace(String arg0) {
-			return null;
-		}
-
-		public String encodeResourceURL(String arg0) {
-			return null;
-		}
-
-		public Map getApplicationMap() {
-			return new HashMap();
-		}
-
-		public String getAuthType() {
-			return null;
-		}
-
-		public Object getContext() {
-			return null;
-		}
-
-		public String getInitParameter(String arg0) {
-			return null;
-		}
-
-		public Map getInitParameterMap() {
-			return null;
-		}
-
-		public String getRemoteUser() {
-			return null;
-		}
-
-		public Object getRequest() {
-			return null;
-		}
-
-		public String getRequestContextPath() {
-			return null;
-		}
-
-		public Map getRequestCookieMap() {
-			return null;
-		}
-
-		public Map getRequestHeaderMap() {
-			return null;
-		}
-
-		public Map getRequestHeaderValuesMap() {
-			return null;
-		}
-
-		public Locale getRequestLocale() {
-			return null;
-		}
-
-		public Iterator getRequestLocales() {
-			return null;
-		}
-
-		public Map getRequestMap() {
-			return null;
-		}
-
-		public Map getRequestParameterMap() {
-			return new HashMap();
-		}
-
-		public Iterator getRequestParameterNames() {
-			return null;
-		}
-
-		public Map getRequestParameterValuesMap() {
-			return null;
-		}
-
-		public String getRequestPathInfo() {
-			return null;
-		}
-
-		public String getRequestServletPath() {
-			return null;
-		}
-
-		public URL getResource(String arg0) throws MalformedURLException {
-			return null;
-		}
-
-		public InputStream getResourceAsStream(String arg0) {
-			return null;
-		}
-
-		public Set getResourcePaths(String arg0) {
-			return null;
-		}
-
-		public Object getResponse() {
-			return null;
-		}
-
-		public Object getSession(boolean arg0) {
-			return null;
-		}
-
-		public Map getSessionMap() {
-			return null;
-		}
-
-		public Principal getUserPrincipal() {
-			return null;
-		}
-
-		public boolean isUserInRole(String arg0) {
-			return false;
-		}
-
-		public void log(String arg0) {
-		}
-
-		public void log(String arg0, Throwable arg1) {
-		}
-
-		public void redirect(String arg0) throws IOException {
 		}
 	}
 }
--- spring-projects/spring-webflow/src/test/org/springframework/webflow/manager/jsf/FlowPhaseListenerTests.java
+++ spring-projects/spring-webflow/src/test/org/springframework/webflow/manager/jsf/FlowPhaseListenerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2004 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +15,8 @@
  */
 package org.springframework.webflow.manager.jsf;
 
+import java.util.HashMap;
+
 import javax.faces.FacesException;
 import javax.faces.context.FacesContext;
 import javax.faces.event.PhaseEvent;
@@ -24,31 +26,149 @@ import javax.faces.lifecycle.Lifecycle;
 
 import junit.framework.TestCase;
 
+import org.easymock.MockControl;
+import org.springframework.webflow.ExternalContext;
+import org.springframework.webflow.execution.FlowExecution;
+import org.springframework.webflow.execution.repository.FlowExecutionContinuationKey;
+import org.springframework.webflow.execution.repository.FlowExecutionRepository;
+
 /**
- * Unit test for the FlowPhaseListener class.
+ * Unit tests for the FlowPhaseListener class.
  * 
  * @author Ulrik Sandberg
  */
 public class FlowPhaseListenerTests extends TestCase {
 
-	private FlowPhaseListener tested;
+	private MockControl flowExecutionControl;
+
+	private FlowExecution flowExecutionMock;
+
+	private MockControl flowExecutionRepositoryControl;
+
+	private FlowExecutionRepository flowExecutionRepositoryMock;
 
 	private MockFacesContext mockFacesContext;
 
+	private MockJsfExternalContext mockJsfExternalContext;
+
 	private MyLifecycle lifecycle;
+
+	private FlowExecutionHolder flowExecutionHolder;
+
+	private FlowExecutionContinuationKey flowExecutionContinuationKey;
+
+	private FlowPhaseListener tested;
 
 	protected void setUp() throws Exception {
 		super.setUp();
+		flowExecutionControl = MockControl.createControl(FlowExecution.class);
+		flowExecutionMock = (FlowExecution)flowExecutionControl.getMock();
+
+		flowExecutionRepositoryControl = MockControl.createControl(FlowExecutionRepository.class);
+		flowExecutionRepositoryMock = (FlowExecutionRepository)flowExecutionRepositoryControl.getMock();
+
 		mockFacesContext = new MockFacesContext();
+		mockJsfExternalContext = new MockJsfExternalContext();
+		mockFacesContext.setExternalContext(mockJsfExternalContext);
 		lifecycle = new MyLifecycle();
+
+		flowExecutionHolder = new FlowExecutionHolder(flowExecutionMock);
+		flowExecutionContinuationKey = new FlowExecutionContinuationKey("some conversation id", "some continuation id");
+		tested = new FlowPhaseListener() {
+			protected FlowExecutionRepository getRepository(ExternalContext context) {
+				return flowExecutionRepositoryMock;
+			}
+		};
 	}
 
-	public void testBeforePhase() {
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		flowExecutionControl = null;
+		flowExecutionMock = null;
+
+		flowExecutionRepositoryControl = null;
+		flowExecutionRepositoryMock = null;
+
+		flowExecutionHolder = null;
+
+		mockFacesContext = null;
+		mockJsfExternalContext = null;
+
+		lifecycle = null;
+		tested = null;
+	}
+
+	protected void replay() {
+		flowExecutionControl.replay();
+		flowExecutionRepositoryControl.replay();
+	}
+
+	protected void verify() {
+		flowExecutionControl.verify();
+		flowExecutionRepositoryControl.verify();
+	}
+
+	public void testBeforePhaseNullHolder() {
 		PhaseEvent event = new PhaseEvent(mockFacesContext, PhaseId.RENDER_RESPONSE, lifecycle);
+		replay();
+
+		// perform test
 		tested.beforePhase(event);
+
+		verify();
+	}
+
+	public void testBeforePhaseNonNullHolder() {
+		PhaseEvent event = new PhaseEvent(mockFacesContext, PhaseId.RENDER_RESPONSE, lifecycle);
+		HashMap requestMap = new HashMap();
+		requestMap.put(FlowExecutionHolder.class.getName(), flowExecutionHolder);
+		mockJsfExternalContext.setRequestMap(requestMap);
+
+		// preparing holder with a null key
+		flowExecutionHolder.setContinuationKey(null);
+
+		flowExecutionControl.expectAndReturn(flowExecutionMock.isActive(), true);
+		flowExecutionRepositoryControl.expectAndReturn(flowExecutionRepositoryMock
+				.generateContinuationKey(flowExecutionMock), flowExecutionContinuationKey);
+		replay();
+
+		// perform test
+		tested.beforePhase(event);
+
+		verify();
+		assertEquals(flowExecutionContinuationKey, flowExecutionHolder.getContinuationKey());
+		assertEquals("_ssome conversation id_csome continuation id", requestMap
+				.get(FlowPhaseListener.FLOW_EXECUTION_ID_ATTRIBUTE));
+		assertEquals(flowExecutionMock, requestMap.get(FlowPhaseListener.FLOW_EXECUTION_CONTEXT_ATTRIBUTE));
+	}
+
+	public void testBeforePhaseNonNullHolderNonNullKey() {
+		PhaseEvent event = new PhaseEvent(mockFacesContext, PhaseId.RENDER_RESPONSE, lifecycle);
+		HashMap requestMap = new HashMap();
+		requestMap.put(FlowExecutionHolder.class.getName(), flowExecutionHolder);
+		mockJsfExternalContext.setRequestMap(requestMap);
+
+		// preparing holder with a key
+		flowExecutionHolder
+				.setContinuationKey(new FlowExecutionContinuationKey("some conversation id", "should change"));
+
+		flowExecutionControl.expectAndReturn(flowExecutionMock.isActive(), true);
+		flowExecutionRepositoryControl.expectAndReturn(flowExecutionRepositoryMock.generateContinuationKey(
+				flowExecutionMock, "some conversation id"), flowExecutionContinuationKey);
+		replay();
+
+		// perform test
+		tested.beforePhase(event);
+
+		verify();
+		assertEquals(flowExecutionContinuationKey, flowExecutionHolder.getContinuationKey());
+		assertEquals("_ssome conversation id_csome continuation id", requestMap
+				.get(FlowPhaseListener.FLOW_EXECUTION_ID_ATTRIBUTE));
+		assertEquals(flowExecutionMock, requestMap.get(FlowPhaseListener.FLOW_EXECUTION_CONTEXT_ATTRIBUTE));
 	}
 
 	public void testAfterPhaseNullEvent() {
+		replay();
 		try {
 			tested.afterPhase(null);
 			fail("NullPointerException expected");
@@ -56,25 +176,71 @@ public class FlowPhaseListenerTests extends TestCase {
 		catch (NullPointerException expected) {
 			// expected
 		}
+		verify();
 	}
 
 	public void testAfterPhaseNotRestoreNotRender() {
 		PhaseEvent event = new PhaseEvent(mockFacesContext, PhaseId.ANY_PHASE, lifecycle);
+		replay();
+
+		// perform test
 		tested.afterPhase(event);
+
+		verify();
 	}
 
 	public void testAfterPhaseRestoreView() {
 		PhaseEvent event = new PhaseEvent(mockFacesContext, PhaseId.RESTORE_VIEW, lifecycle);
+		replay();
 
 		// perform test
 		tested.afterPhase(event);
 
+		verify();
+	}
+
+	public void testAfterPhaseRestoreViewFlowExecutionIdHasText() {
+		PhaseEvent event = new PhaseEvent(mockFacesContext, PhaseId.RESTORE_VIEW, lifecycle);
+		HashMap requestParameterMap = new HashMap();
+		requestParameterMap.put("_flowExecutionId", "_ssome conversation id_csome continuation id");
+		mockJsfExternalContext.setRequestParameterMap(requestParameterMap);
+		flowExecutionRepositoryControl.expectAndReturn(flowExecutionRepositoryMock
+				.getFlowExecution(flowExecutionContinuationKey), flowExecutionMock);
+		replay();
+
+		// perform test
+		tested.afterPhase(event);
+
+		verify();
 	}
 
 	public void testAfterPhaseRenderResponse() {
 		PhaseEvent event = new PhaseEvent(mockFacesContext, PhaseId.RENDER_RESPONSE, lifecycle);
+		replay();
+
 		// perform test
 		tested.afterPhase(event);
+
+		verify();
+	}
+
+	public void testAfterPhaseRenderResponseNonNullHolder() {
+		PhaseEvent event = new PhaseEvent(mockFacesContext, PhaseId.RENDER_RESPONSE, lifecycle);
+		HashMap requestMap = new HashMap();
+		requestMap.put(FlowExecutionHolder.class.getName(), flowExecutionHolder);
+		mockJsfExternalContext.setRequestMap(requestMap);
+
+		// preparing holder with a key
+		flowExecutionHolder.setContinuationKey(flowExecutionContinuationKey);
+
+		flowExecutionControl.expectAndReturn(flowExecutionMock.isActive(), true);
+		flowExecutionRepositoryMock.putFlowExecution(flowExecutionContinuationKey, flowExecutionMock);
+		replay();
+
+		// perform test
+		tested.afterPhase(event);
+
+		verify();
 	}
 
 	private static class MyLifecycle extends Lifecycle {

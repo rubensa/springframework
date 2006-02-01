749f6d9173fce869b5fc2dba2916e513576fec9f
kdonald <>
2006-02-01 06:25:57 +0000

    polish

--- spring-projects/spring-webflow/src/java/org/springframework/webflow/execution/repository/SimpleFlowExecutionRepositoryFactory.java
+++ spring-projects/spring-webflow/src/java/org/springframework/webflow/execution/repository/SimpleFlowExecutionRepositoryFactory.java
@@ -2,14 +2,6 @@ package org.springframework.webflow.execution.repository;
 
 import org.springframework.webflow.execution.FlowLocator;
 
-/**
- * A repository factory that returns the same (singleton) instance of a
- * {@link FlowExecutionRepository} on each invocation. Designed to be used with
- * {@link FlowExecutionRepository} implementations that are stateless and
- * therefore shareable by all threads.
- * 
- * @author Keith Donald
- */
 public class SimpleFlowExecutionRepositoryFactory extends DelegatingFlowExecutionRepositoryFactory {
 
 	public SimpleFlowExecutionRepositoryFactory(FlowLocator flowLocator) {

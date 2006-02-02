eda53f1a6c98846b71b362698c1260dc5ace84b6
kdonald <>
2006-02-02 20:33:49 +0000

    javadoc

--- spring-projects/spring-webflow/src/java/org/springframework/webflow/support/StateIdExpressionTransitionTargetStateResolver.java
+++ spring-projects/spring-webflow/src/java/org/springframework/webflow/support/StateIdExpressionTransitionTargetStateResolver.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2002-2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.webflow.support;
 
 import java.util.HashMap;
@@ -9,18 +24,33 @@ import org.springframework.webflow.State;
 import org.springframework.webflow.Transition;
 import org.springframework.webflow.TransitionTargetStateResolver;
 
+/**
+ * Resolves the target state of a transition by using the String-value returned
+ * from evaluating an {@link Expression expression} against the flow execution
+ * {@link RequestContext request context}.
+ * 
+ * @author Keith Donald
+ */
 public class StateIdExpressionTransitionTargetStateResolver implements TransitionTargetStateResolver {
 
 	private static final String LAST_STATE_ALIAS = "lastState";
-	
-	private Expression stateIdExpression;
-	
-	public StateIdExpressionTransitionTargetStateResolver(Expression stateIdExpression) {
-		this.stateIdExpression = stateIdExpression;
+
+	/**
+	 * The expression to evaluate, expected to return a String used as the
+	 * stateId.
+	 */
+	private Expression expression;
+
+	/**
+	 * Creates a new expression-based transition target state resolver.
+	 * @param expression the expression
+	 */
+	public StateIdExpressionTransitionTargetStateResolver(Expression expression) {
+		this.expression = expression;
 	}
-	
+
 	public State resolveTargetState(Transition transition, RequestContext context) {
-		String stateId = (String)stateIdExpression.evaluateAgainst(context, getEvaluationContext(context));
+		String stateId = String.valueOf(expression.evaluateAgainst(context, getEvaluationContext(context)));
 		return transition.getSourceState().getFlow().getRequiredState(stateId);
 	}
 

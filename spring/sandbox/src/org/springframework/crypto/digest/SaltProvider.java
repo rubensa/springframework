302a62eb2caf16a26c55dddd3d76e3fe0c2cf007
cvs2git <admin@example.com>
2006-02-11 10:34:48 +0000

    This commit was manufactured by cvs2git to create tag 'release-spring- ws-0-9'.

--- spring/sandbox/src/org/springframework/crypto/digest/SaltProvider.java
+++
@@ -1,8 +0,0 @@
-package org.springframework.crypto.digest;
-
-/**
- * @author Rob Harrop
- */
-public interface SaltProvider {
-    byte[] newSalt(int size);
-}
--- spring/test/org/springframework/scripting/Calculator.java
+++
@@ -1,9 +0,0 @@
-package org.springframework.scripting;
-
-/**
- * @author robh
- */
-public interface Calculator {
-
-    int add(int x, int y);
-}

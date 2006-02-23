2ce119352a015f833f0731a4d9f7bbb87e6b9a8e
poutsma <>
2006-02-23 12:45:22 +0000

    Fixing SWS-5: Added XML validation abstraction.

--- spring-projects/spring-ws-core/src/java/org/springframework/ws/endpoint/PayloadValidatingInterceptor.java
+++ spring-projects/spring-ws-core/src/java/org/springframework/ws/endpoint/PayloadValidatingInterceptor.java
@@ -17,30 +17,21 @@
 package org.springframework.ws.endpoint;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import javax.xml.XMLConstants;
 import javax.xml.namespace.QName;
-import javax.xml.transform.Source;
-import javax.xml.transform.stream.StreamSource;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
-import javax.xml.validation.Validator;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.core.io.Resource;
 import org.springframework.util.Assert;
-import org.springframework.ws.WebServiceMessage;
+import org.springframework.util.ObjectUtils;
 import org.springframework.ws.context.MessageContext;
 import org.springframework.ws.soap.SoapEndpointInterceptor;
 import org.springframework.ws.soap.SoapMessage;
 import org.springframework.ws.soap.context.SoapMessageContext;
+import org.springframework.xml.validation.XmlValidator;
+import org.springframework.xml.validation.XmlValidatorFactory;
 import org.w3c.dom.Element;
-import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 
@@ -64,7 +55,7 @@ public class PayloadValidatingInterceptor implements SoapEndpointInterceptor, In
 
     private static final Log logger = LogFactory.getLog(PayloadValidatingInterceptor.class);
 
-    private String schemaLanguage = XMLConstants.W3C_XML_SCHEMA_NS_URI;
+    private String schemaLanguage = XmlValidatorFactory.SCHEMA_W3C_XML;
 
     private Resource schemaResource;
 
@@ -72,7 +63,7 @@ public class PayloadValidatingInterceptor implements SoapEndpointInterceptor, In
 
     private boolean validateResponse = false;
 
-    private Schema schema;
+    private XmlValidator validator;
 
     /**
      * Sets the schema resource to use for validation.
@@ -84,8 +75,8 @@ public class PayloadValidatingInterceptor implements SoapEndpointInterceptor, In
     /**
      * Sets the schema language. Default is the W3C XML Schema: <code>http://www.w3.org/2001/XMLSchema"</code>.
      *
-     * @see XMLConstants#W3C_XML_SCHEMA_NS_URI
-     * @see XMLConstants#RELAXNG_NS_URI
+     * @see XmlValidatorFactory#SCHEMA_W3C_XML
+     * @see XmlValidatorFactory#SCHEMA_RELAX_NG
      */
     public void setSchemaLanguage(String schemaLanguage) {
         this.schemaLanguage = schemaLanguage;
@@ -119,11 +110,10 @@ public class PayloadValidatingInterceptor implements SoapEndpointInterceptor, In
      */
     public boolean handleRequest(MessageContext messageContext, Object endpoint) throws IOException, SAXException {
         if (validateRequest) {
-            List errors = validate(messageContext.getRequest());
-            if (!errors.isEmpty()) {
-                for (Iterator iterator = errors.iterator(); iterator.hasNext();) {
-                    SAXParseException ex = (SAXParseException) iterator.next();
-                    logger.warn("XML validation error on request: " + ex.getMessage());
+            SAXParseException[] errors = validator.validate(messageContext.getRequest().getPayloadSource());
+            if (!ObjectUtils.isEmpty(errors)) {
+                for (int i = 0; i < errors.length; i++) {
+                    logger.warn("XML validation error on request: " + errors[i].getMessage());
                 }
                 if (messageContext instanceof SoapMessageContext) {
                     SoapMessage response = ((SoapMessageContext) messageContext).createSoapResponse();
@@ -151,11 +141,10 @@ public class PayloadValidatingInterceptor implements SoapEndpointInterceptor, In
      */
     public boolean handleResponse(MessageContext messageContext, Object endpoint) throws IOException, SAXException {
         if (validateResponse) {
-            List errors = validate(messageContext.getResponse());
-            if (!errors.isEmpty()) {
-                for (Iterator iterator = errors.iterator(); iterator.hasNext();) {
-                    SAXParseException ex = (SAXParseException) iterator.next();
-                    logger.error("XML validation error on response: " + ex.getMessage());
+            SAXParseException[] errors = validator.validate(messageContext.getResponse().getPayloadSource());
+            if (!ObjectUtils.isEmpty(errors)) {
+                for (int i = 0; i < errors.length; i++) {
+                    logger.error("XML validation error on response: " + errors[i].getMessage());
                 }
                 return false;
             }
@@ -168,15 +157,7 @@ public class PayloadValidatingInterceptor implements SoapEndpointInterceptor, In
 
     public void afterPropertiesSet() throws Exception {
         Assert.notNull(schemaResource, "schema is required");
-        SchemaFactory schemaFactory = SchemaFactory.newInstance(schemaLanguage);
-        InputStream inputStream = schemaResource.getInputStream();
-        try {
-            Source schemaSource = new StreamSource(inputStream);
-            schema = schemaFactory.newSchema(schemaSource);
-        }
-        finally {
-            inputStream.close();
-        }
+        validator = XmlValidatorFactory.createValidator(schemaResource, schemaLanguage);
     }
 
     /**
@@ -191,38 +172,5 @@ public class PayloadValidatingInterceptor implements SoapEndpointInterceptor, In
      */
     public boolean understands(Element header) {
         return false;
-    }
-
-    private List validate(WebServiceMessage message) throws IOException, SAXException {
-        ValidationErrorHandler errorHandler = new ValidationErrorHandler();
-        Validator validator = schema.newValidator();
-        validator.setErrorHandler(errorHandler);
-        validator.validate(message.getPayloadSource());
-        return errorHandler.getErrors();
-    }
-
-    /**
-     * <code>ErrorHandler</code> implementation that logs warnings to the <code>PayloadValidatingInterceptor</code>
-     * logger, and stores errors and fatal errors in a list.
-     */
-    private static class ValidationErrorHandler implements ErrorHandler {
-
-        private List errors = new ArrayList();
-
-        public List getErrors() {
-            return errors;
-        }
-
-        public void warning(SAXParseException ex) throws SAXException {
-            logger.debug("Ignored XML validation warning: " + ex.getMessage());
-        }
-
-        public void error(SAXParseException ex) throws SAXException {
-            errors.add(ex);
-        }
-
-        public void fatalError(SAXParseException ex) throws SAXException {
-            errors.add(ex);
-        }
     }
 }

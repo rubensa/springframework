47f2ec2ae3f472f129421023c732dcf353d2f597
kdonald <>
2006-02-24 17:15:14 +0000

    polish

--- spring-projects/spring-webflow/src/java/org/springframework/webflow/support/DefaultExpressionParserFactory.java
+++ spring-projects/spring-webflow/src/java/org/springframework/webflow/support/DefaultExpressionParserFactory.java
@@ -1,9 +1,33 @@
+/*
+ * Copyright 2002-2005 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.webflow.support;
 
 import org.springframework.binding.expression.ExpressionParser;
 import org.springframework.binding.expression.support.OgnlExpressionParser;
 
-public class DefaultExpressionParserFactory extends OgnlExpressionParser {
+/**
+ * Factory that creates instances of the default expression parser used by
+ * Spring Web Flow when requested.
+ * <p>
+ * Returns {@link OgnlExpressionParser} by default. Also asserts that OGNL is in
+ * the classpath when this class is loaded.
+ * 
+ * @author Keith Donald
+ */
+public class DefaultExpressionParserFactory {
 
 	static {
 		try {
@@ -17,7 +41,11 @@ public class DefaultExpressionParserFactory extends OgnlExpressionParser {
 		}
 	}
 
+	/**
+	 * Returns the default expression parser.
+	 * @return the expression parser
+	 */
 	public ExpressionParser getExpressionParser() {
 		return new WebFlowOgnlExpressionParser();
-	}	
+	}
 }
--- spring-projects/spring-webflow/src/java/org/springframework/webflow/support/WebFlowOgnlExpressionParser.java
+++ spring-projects/spring-webflow/src/java/org/springframework/webflow/support/WebFlowOgnlExpressionParser.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2002-2005 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.webflow.support;
 
 import java.util.Map;
@@ -9,13 +24,25 @@ import org.springframework.binding.expression.support.OgnlExpressionParser;
 import org.springframework.webflow.AttributeMap;
 import org.springframework.webflow.MapAdaptable;
 
+/**
+ * An extension of {@link OgnlExpressionParser} that registers webflow-specific
+ * property accessors.
+ * @author Keith Donald
+ */
 public class WebFlowOgnlExpressionParser extends OgnlExpressionParser {
 
+	/**
+	 * Creates a webflow-specific ognl expression parser.
+	 */
 	public WebFlowOgnlExpressionParser() {
 		addPropertyAccessor(MapAdaptable.class, new MapAdaptablePropertyAccessor());
 		addPropertyAccessor(AttributeMap.class, new AttributeMapPropertyAccessor());
 	}
 
+	/**
+	 * The {@link MapAdaptable} property accessor.
+	 * @author Keith Donald
+	 */
 	private static class MapAdaptablePropertyAccessor implements PropertyAccessor {
 		public Object getProperty(Map context, Object target, Object name) throws OgnlException {
 			return ((MapAdaptable)target).getMap().get(name);
@@ -27,6 +54,10 @@ public class WebFlowOgnlExpressionParser extends OgnlExpressionParser {
 		}
 	}
 
+	/**
+	 * The {@link AttributeMap} property accessor.
+	 * @author Keith Donald
+	 */
 	private static class AttributeMapPropertyAccessor extends MapAdaptablePropertyAccessor {
 		public void setProperty(Map context, Object target, Object name, Object value) throws OgnlException {
 			((AttributeMap)target).put((String)name, value);

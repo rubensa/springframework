a9ab2f3fccbde325caecdd9f7126535920f5ae84
kdonald <>
2006-02-25 13:39:53 +0000

    *** empty log message ***

--- spring-projects/spring-webflow-samples/itemlist/src/java/org/springframework/webflow/samples/itemlist/DataMapper.java
+++ spring-projects/spring-webflow-samples/itemlist/src/java/org/springframework/webflow/samples/itemlist/DataMapper.java
@@ -2,11 +2,11 @@ package org.springframework.webflow.samples.itemlist;
 
 import org.springframework.binding.mapping.DefaultAttributeMapper;
 import org.springframework.binding.mapping.MappingBuilder;
-import org.springframework.webflow.util.ExpressionUtils;
+import org.springframework.webflow.support.DefaultExpressionParserFactory;
 
 public class DataMapper extends DefaultAttributeMapper {
 	public DataMapper() {
-		MappingBuilder mapping = new MappingBuilder(ExpressionUtils.getDefaultExpressionParser());
+		MappingBuilder mapping = new MappingBuilder(new DefaultExpressionParserFactory().getExpressionParser());
 		addMapping(mapping.source("requestParameters.data").target("flowScope.item").value());
 	}
 }

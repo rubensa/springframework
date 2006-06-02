fa0b9ba3ead162391278a4d922e00f8c3a04a373
kdonald <>
2006-06-02 20:18:39 +0000

    *** empty log message ***

--- spring-projects/spring-binding/src/test/org/springframework/binding/method/ClassMethodKeyTests.java
+++ spring-projects/spring-binding/src/test/org/springframework/binding/method/ClassMethodKeyTests.java
@@ -16,11 +16,11 @@
 
 package org.springframework.binding.method;
 
-import junit.framework.TestCase;
-
 import java.io.File;
 import java.io.FilenameFilter;
 import java.lang.reflect.Method;
+
+import junit.framework.TestCase;
 
 /**
  * @author Rob Harrop
@@ -28,38 +28,44 @@ import java.lang.reflect.Method;
  */
 public class ClassMethodKeyTests extends TestCase {
 
-    private static final Method LIST_NO_ARGS = safeGetMethod(File.class, "list", null);
+	private static final Method LIST_NO_ARGS = safeGetMethod(File.class, "list", null);
 
-    private static final Method LIST_FILENAME_FILTER = safeGetMethod(File.class, "list", new Class[]{FilenameFilter.class});
+	private static final Method LIST_FILENAME_FILTER = safeGetMethod(File.class, "list",
+			new Class[] { FilenameFilter.class });
 
-    public void testGetMethodWithNoArgs() throws Exception {
-        ClassMethodKey key = new ClassMethodKey(File.class, "list", new Class[0]);
-        Method m = key.getMethod();
-        assertEquals(LIST_NO_ARGS, m);
-    }
+	public void testGetMethodWithNoArgs() throws Exception {
+		ClassMethodKey key = new ClassMethodKey(File.class, "list", new Class[0]);
+		Method m = key.getMethod();
+		assertEquals(LIST_NO_ARGS, m);
+	}
 
-    public void testGetMethodWithSingleArg() throws Exception {
-        ClassMethodKey key = new ClassMethodKey(File.class, "list", new Class[]{FilenameFilter.class});
-        Method m = key.getMethod();
-        assertEquals(LIST_FILENAME_FILTER, m);
-    }
+	public void testGetMoreGenericMethod() throws Exception {
+		ClassMethodKey key = new ClassMethodKey(Object.class, "equals", new Class[] { Long.class });
+		assertEquals(safeGetMethod(Object.class, "equals", new Class[] { Object.class }), key.getMethod());
+	}
 
-    public void testGetMethodWithSingleNullArgAndValidMatch() throws Exception {
-        ClassMethodKey key = new ClassMethodKey(File.class, "list", new Class[]{null});
-        Method m = key.getMethod();
-        assertEquals(LIST_FILENAME_FILTER, m);
-    }
+	public void testGetMethodWithSingleArg() throws Exception {
+		ClassMethodKey key = new ClassMethodKey(File.class, "list", new Class[] { FilenameFilter.class });
+		Method m = key.getMethod();
+		assertEquals(LIST_FILENAME_FILTER, m);
+	}
 
-    public void testGetMethodWithSingleNullAndUnclearMatch() throws Exception {
-       ClassMethodKey key = new ClassMethodKey(File.class, "listFiles", new Class[]{null});
-    }
+	public void testGetMethodWithSingleNullArgAndValidMatch() throws Exception {
+		ClassMethodKey key = new ClassMethodKey(File.class, "list", new Class[] { null });
+		Method m = key.getMethod();
+		assertEquals(LIST_FILENAME_FILTER, m);
+	}
 
-    private static final Method safeGetMethod(Class type, String name, Class[] argTypes) {
-        try {
-            return type.getMethod(name, argTypes);
-        }
-        catch (NoSuchMethodException e) {
-            throw new IllegalStateException("Unable to safely access a known method via reflection. " + e.getMessage());
-        }
-    }
+	public void testGetMethodWithSingleNullAndUnclearMatch() throws Exception {
+		ClassMethodKey key = new ClassMethodKey(File.class, "listFiles", new Class[] { null });
+	}
+
+	private static final Method safeGetMethod(Class type, String name, Class[] argTypes) {
+		try {
+			return type.getMethod(name, argTypes);
+		}
+		catch (NoSuchMethodException e) {
+			throw new IllegalStateException("Unable to safely access a known method via reflection. " + e.getMessage());
+		}
+	}
 }

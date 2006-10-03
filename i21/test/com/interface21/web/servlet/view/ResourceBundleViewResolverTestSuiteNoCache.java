fc6d910382a522cc1f20e48e600d146d0a2a8ed6
cvs2git <admin@example.com>
2006-10-03 12:46:25 +0000

    This commit was manufactured by cvs2git to create tag 'release-2-0'.

--- i21/test/com/interface21/web/servlet/view/ResourceBundleViewResolverTestSuiteNoCache.java
+++
@@ -1,9 +0,0 @@
-package com.interface21.web.servlet.view;
-
-public class ResourceBundleViewResolverTestSuiteNoCache extends ResourceBundleViewResolverTestSuite {
-	
-	protected boolean getCache() {
-		return false;
-	}
-}
- 

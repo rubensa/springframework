6d84a3510e1c711ff3f6d7d1a6da495025337f1e
sbrannen <>
2007-09-04 09:38:49 +0000

    Revising test annotations: 1) Method-level test annotations are no longer inappropriately annotated with @Inherited. 2) Removed unnecessary imports. 3) Updated copyright year.

--- spring/tiger/mock/org/springframework/test/annotation/Rollback.java
+++ spring/tiger/mock/org/springframework/test/annotation/Rollback.java
@@ -18,7 +18,6 @@ package org.springframework.test.annotation;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -35,7 +34,6 @@ import java.lang.annotation.Target;
  */
 @Target( { ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
-@Inherited
 @Documented
 public @interface Rollback {
 
